16cbfc25102bbf03592a2a4a5070616472f7610d
tpair24 <tpair24@thycotic.com>
2015-01-28 15:52:35 -0500

    Changed assembly info

---
+++ ProductInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Log")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
