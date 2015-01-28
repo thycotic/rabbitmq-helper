5e53061c043158b9a536054990d2b5448b7e435a
tpair24 <tpair24@thycotic.com>
2015-01-28 15:18:54 -0500

    Documentation

--- Thycotic.Utility/TypeHelper.cs
+++ Thycotic.Utility/TypeHelper.cs
@@ -3,9 +3,18 @@ using System.Linq;
 
 namespace Thycotic.Utility
 {
+    /// <summary>
+    /// Type helper methods
+    /// </summary>
     public static class TypeHelper
     {
         //source: http://stackoverflow.com/questions/74616/how-to-detect-if-type-is-another-generic-type/1075059#1075059
+        /// <summary>
+        /// Determines whether the specified type is assignable to the specified generic.
+        /// </summary>
+        /// <param name="givenType">Type of the given.</param>
+        /// <param name="genericType">Type of the generic.</param>
+        /// <returns></returns>
         public static bool IsAssignableToGenericType(this Type givenType, Type genericType)
         {
             var interfaceTypes = givenType.GetInterfaces();
