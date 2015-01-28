111118ea5340c92e9a14e4a07b1199eb84b08fae
tpair24 <tpair24@thycotic.com>
2015-01-28 09:34:13 -0500

    Revert "Clean up. Not needing a wrappers project."
    This reverts commit b2b4452f9c32d3be9cd4dac61f1987d372b639c5.

---
+++ Thycotic.MessageQueueClient.Wrappers/AutofacRpcConsumer.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using Thycotic.Logging;
+
+namespace Thycotic.MessageQueueClient.RabbitMq
+{
+    public class AutofacRpcConsumer<TMsg, TResponse, THandler> : ConsumerWrapperBase<TMsg, THandler>
+        where THandler : IConsumer<TMsg, TResponse>
+    {
+        //private readonly Func<Owned<IConsume<TMsg, TResponse>>> _handlerFactory;
+        private readonly IMessageSerializer _serializer;
+        private readonly IRabbitMqConnection _rmq;
+        //private readonly IActivityMonitor _monitor;
+        private readonly ILogWriter _log = Log.Get(typeof (AutofacRpcConsumer<TMsg, TResponse, THandler>));
+
+        public AutofacRpcConsumer(IMessageSerializer serializer, IRabbitMqConnection rmq)
+            : base(rmq)
+        {
+
+            _serializer = serializer;
+            _rmq = rmq;
+
+        }
+
+         public override void HandleBasicDeliver(string consumerTag, ulong deliveryTag, bool redelivered, string exchange,
+            string routingKey, IBasicProperties properties, byte[] body)
+        {
+            Task.Run(() => ExecuteMessage(deliveryTag, properties, body));
+        }
+
+        public void ExecuteMessage(ulong deliveryTag, IBasicProperties properties, byte[] body)
+        {
+            try
+            {
+
+                var message = _serializer.BytesToMessage<TMsg>(body);
+                var responseType = "success";
+                object response;
+
+                //using (var handler = _handlerFactory())
+                //{
+                //    try
+                //    {
+                //        response = handler.Value.Consume(message);
+                //    }
+                //    catch (Exception e)
+                //    {
+                //        _log.Error("Handler error", e);
+                //        response = new RpcError {Message = e.Message};
+                //        responseType = "error";
+                //    }
+                //}
+
+                //if (properties.IsReplyToPresent())
+                //{
+                //    Respond(properties.ReplyTo, response, properties.CorrelationId, responseType);
+                //}
+            }
+            catch (Exception e)
+            {
+                _log.Error("Failed to handle message " + typeof (TMsg).Name, e);
+            }
+            finally
+            {
+                Model.BasicAck(deliveryTag, false);
+            }
+        }
+
+        private void Respond(string replyTo, object response, string correlationId, string type)
+        {
+            var respBytes = _serializer.MessageToBytes(response);
+            using (var channel = _rmq.OpenChannel(retryAttempts: 9, retryDelayMs: 125, retryDelayGrowthFactor: 2))
+            {
+                channel.ConfirmSelect();
+                var p = channel.CreateBasicProperties();
+                p.CorrelationId = correlationId;
+                p.Type = type;
+                channel.BasicPublish(exchange: "", routingKey: replyTo, mandatory: false, immediate: false,
+                    basicProperties: p,
+                    body: respBytes);
+                channel.WaitForConfirmsOrDie(DefaultConfigValues.ConfirmationTimeout);
+            }
+        }
+
+    }
+
+}
