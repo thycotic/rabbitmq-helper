720dce0206bd3a709af7749e678de113cc53f684
tpair24 <tpair24@thycotic.com>
2015-01-28 14:09:32 -0500

    Throwing messae and consumer

---
+++ Thycotic.Messages/Areas/POC/Request/ThrowingRpcMessage.cs
@@ -0,0 +1,10 @@
+﻿using Thycotic.MessageQueueClient;
+
+namespace Thycotic.Messages.Areas.POC.Request
+{
+    public class ThrowingRpcMessage : IConsumable
+    {
+        public int Version { get; set; }
+        public int RetryCount { get; set; }
+    }
+}
---
+++ Thycotic.SecretServerAgent2.Logic/Areas/POC/ThrowingRpcConsumer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using Thycotic.Logging;
+using Thycotic.MessageQueueClient;
+using Thycotic.Messages.Areas.POC.Request;
+using Thycotic.Messages.Common;
+
+namespace Thycotic.SecretServerAgent2.Logic.Areas.POC
+{
+    public class ThrowingRpcConsumer : IRpcConsumer<ThrowingRpcMessage, RpcResult>
+    {
+        private readonly ILogWriter _log = Log.Get(typeof(ThrowingRpcConsumer));
+
+        public RpcResult Consume(ThrowingRpcMessage request)
+        {
+            _log.Error("Throwing!");
+            throw new ApplicationException("Throw your hands up in the air!");
+        }
+    }
+}
