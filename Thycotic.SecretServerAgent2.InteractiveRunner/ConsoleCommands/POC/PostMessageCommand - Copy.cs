3050728854cc7a8a4892638ac358940b92441f85
Dobroslav Kolev <dobroslav.kolev@thycotic.com>
2015-01-29 00:08:07 -0500

    Chain message

---
+++ Thycotic.SecretServerAgent2.InteractiveRunner/ConsoleCommands/POC/PostMessageCommand - Copy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thycotic.Logging;
+using Thycotic.MessageQueueClient;
+using Thycotic.Messages.Areas.POC.Request;
+
+namespace Thycotic.SecretServerAgent2.InteractiveRunner.ConsoleCommands.POC
+{
+    class ChainMessageCommand : ConsoleCommandBase
+    {
+        private readonly IMessageBus _bus;
+        private readonly ILogWriter _log = Log.Get(typeof(ChainMessageCommand));
+
+        public override string Name
+        {
+            get { return "chainmessage"; }
+        }
+
+        public override string Description
+        {
+            get { return "Posts a chain message to the exchange"; }
+        }
+
+        public ChainMessageCommand(IMessageBus bus)
+        {
+            _bus = bus;
+
+            Action = parameters =>
+            {
+                _log.Info("Posting message to change");
+
+                var message = new ChainMessage();
+
+                _bus.Publish(message);
+
+                _log.Info("Posting completed");
+
+            };
+        }
+    }
+}
---
+++ Thycotic.SecretServerAgent2.Logic/Areas/POC/ChainMessageConsumer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Thycotic.Logging;
+using Thycotic.MessageQueueClient;
+using Thycotic.Messages.Areas.POC.Request;
+using Thycotic.Messages.Common;
+
+namespace Thycotic.SecretServerAgent2.Logic.Areas.POC
+{
+    /// <summary>
+    /// Simple Hello World consumer
+    /// </summary>
+    public class ChainMessageConsumer : IConsumer<ChainMessage>
+    {
+        private readonly IMessageBus _bus;
+        private readonly ILogWriter _log = Log.Get(typeof(ChainMessage));
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChainMessageConsumer"/> class.
+        /// </summary>
+        /// <param name="bus">The bus.</param>
+        public ChainMessageConsumer(IMessageBus bus)
+        {
+            _bus = bus;
+        }
+
+        /// <summary>
+        /// Consumes the specified request.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        public void Consume(ChainMessage request)
+        {
+            _log.Info("CONSUMER: Received chain message");
+            //throw new ApplicationException();
+
+            Console.WriteLine("Posting next message...");
+
+            _bus.Publish(new HelloWorldMessage { Content = "Hello from the chain consumer!"});
+
+            Console.WriteLine("Posted");
+        }
+    }
+}
