e1741c0f35e18d13a5deff41d579078c8b695734
Dobroslav Kolev <dobroslav.kolev@thycotic.com>
2015-02-05 14:33:43 -0500

    Engine verification nagging that engined are not being verified

---
+++ Thycotic.SecretServerEngine2/MemoryMq/EngineVerifier.cs
@@ -0,0 +1,18 @@
+﻿using System.IdentityModel.Selectors;
+using Thycotic.Logging;
+
+namespace Thycotic.SecretServerEngine2.MemoryMq
+{
+    internal class EngineVerifier : UserNamePasswordValidator
+    {
+        private readonly ILogWriter _log = Log.Get(typeof(EngineVerifier));
+
+        public override void Validate(string agentkey, string ignored)
+        {
+            _log.Info(string.Format("Engine with key {0} connected", agentkey));
+            _log.Warn("Engine validation is inactive");
+
+
+        }
+    }
+}
