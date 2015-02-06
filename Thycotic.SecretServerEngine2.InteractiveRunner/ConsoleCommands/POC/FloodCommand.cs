feb7c0c8e2a91a13622dc0140e2d0aef7cad7a07
Dobroslav Kolev <dobroslav.kolev@thycotic.com>
2015-02-06 11:57:23 -0500

    Consolidating flood command.

--- Thycotic.SecretServerEngine2.InteractiveRunner/ConsoleCommands/POC/FloodCommand.cs
+++ Thycotic.SecretServerEngine2.InteractiveRunner/ConsoleCommands/POC/FloodCommand.cs
@@ -1,26 +1,27 @@
 ﻿using System;
+using System.Linq;
 using Thycotic.Logging;
 using Thycotic.MessageQueueClient;
 using Thycotic.Messages.Areas.POC.Request;
 
 namespace Thycotic.SecretServerEngine2.InteractiveRunner.ConsoleCommands.POC
 {
-    class FloodInSerialCommand : ConsoleCommandBase
+    class FloodCommand : ConsoleCommandBase
     {
         private readonly IRequestBus _bus;
-        private readonly ILogWriter _log = Log.Get(typeof(FloodInSerialCommand));
+        private readonly ILogWriter _log = Log.Get(typeof(FloodCommand));
 
         public override string Name
         {
-            get { return "floodinserial"; }
+            get { return "flood"; }
         }
 
         public override string Description
         {
-            get { return "Floods the exchange in serial"; }
+            get { return "Floods the exchange with messages"; }
         }
 
-        public FloodInSerialCommand(IRequestBus bus)
+        public FloodCommand(IRequestBus bus)
         {
             _bus = bus;
 
@@ -31,17 +32,17 @@ namespace Thycotic.SecretServerEngine2.InteractiveRunner.ConsoleCommands.POC
 
                 var count = Convert.ToInt32(countString);
 
-                _log.Info(string.Format("Flooding exchange in serial with {0} request(s). Please wait...", count));
+                _log.Info(string.Format("Flooding exchange with {0} request(s). Please wait...", count));
 
-                for (var loop = 0; loop < count; loop++)
+                Enumerable.Range(0, count).AsParallel().ForAll(i =>
                 {
                     var message = new HelloWorldMessage
                     {
-                        Content = string.Format("{0} {1}", loop, Guid.NewGuid())
+                        Content = string.Format("{0} {1}", i, Guid.NewGuid())
                     };
 
                     _bus.BasicPublish(message);
-                }
+                });
 
                 _log.Info("Flooding completed");
             };
