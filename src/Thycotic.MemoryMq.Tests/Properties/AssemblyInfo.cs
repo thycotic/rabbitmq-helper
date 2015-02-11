7a9b11d48c4e2c3aca037d7bcbc72ce8949ee86e
tpair24 <tpair24@thycotic.com>
2015-02-11 13:05:50 -0500

    Moving assembly versioning back into each project

--- src/Thycotic.MemoryMq.Tests/Properties/AssemblyInfo.cs
+++ src/Thycotic.MemoryMq.Tests/Properties/AssemblyInfo.cs
@@ -6,20 +6,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Thycotic.MemoryMq.Tests")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Thycotic.MemoryMq.Tests")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ffc28c4c-07e4-4b98-80c1-c7796362ea90")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +17,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.*")]
