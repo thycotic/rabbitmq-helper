ca53e448c7f7dc59fe4ecba7b878e5496019ec7b
tpair24 <tpair24@thycotic.com>
2015-02-11 09:30:12 -0500

    Assembly version and signing clean up

--- Thycotic.SecretServerEngine2.CertificateHelper/Properties/AssemblyInfo.cs
+++ Thycotic.SecretServerEngine2.CertificateHelper/Properties/AssemblyInfo.cs
@@ -1,35 +1,8 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Thycotic.SecretServerEngine2.CertificateHelper")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Thycotic.SecretServerEngine2.CertificateHelper")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("aef1ae38-b593-4b40-a19a-7f8c075f31d9")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
