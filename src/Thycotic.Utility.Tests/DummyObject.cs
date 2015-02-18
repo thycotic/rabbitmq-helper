c24c1374a386f9477c2e8ea9afcc5cb76ca96072
dobroslav.kolev <dobroslav.kolev@thycotic.com>
2015-02-18 15:42:22 -0500

    Dummy object cleanup

--- src/Thycotic.Utility.Tests/DummyObject.cs
+++ src/Thycotic.Utility.Tests/DummyObject.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Thycotic.Utility.Tests
+﻿namespace Thycotic.Utility.Tests
 {
     class DummyObject
     {
         public int Integer { get; set; }
-
-        public String String;
 
         public string StatusText { get; set; }
 
