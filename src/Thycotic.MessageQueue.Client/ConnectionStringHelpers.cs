b4f1706a46ac474444616d33d53fa20db8d2d7aa
Dobroslav Kolev <dobroslav.kolev@thycotic.com>
2015-03-05 12:33:22 -0500

    Wip

---
+++ src/Thycotic.MessageQueue.Client/ConnectionStringHelpers.cs
@@ -0,0 +1,58 @@
+﻿using Thycotic.Utility;
+
+namespace Thycotic.MessageQueue.Client
+{
+    /// <summary>
+    /// Connection string helpers
+    /// </summary>
+    public static class ConnectionStringHelpers
+    {
+        /// <summary>
+        /// Gets the local memory mq connection string.
+        /// </summary>
+        /// <param name="portNumber">The port number.</param>
+        /// <returns></returns>
+        public static string GetLocalMemoryMqConnectionString(int portNumber = DefaultPorts.MemoryMq.NonSsl)
+        {
+            return GetMemoryMqConnectionString(DnsEx.GetDnsHostName(), portNumber);
+        }
+
+        /// <summary>
+        /// Gets the local rabbit mq connection string.
+        /// </summary>
+        /// <param name="hostName">Name of the host.</param>
+        /// <param name="portNumber">The port number.</param>
+        /// <returns></returns>
+        public static string GetLocalRabbitMqConnectionString(string hostName, int portNumber = DefaultPorts.RabbitMq.NonSsl)
+        {
+            return GetRabbitMqConnectionString(DnsEx.GetDnsHostName(), portNumber);
+        }
+
+        /// <summary>
+        /// Gets the memory mq connection string.
+        /// </summary>
+        /// <param name="hostName">Name of the host.</param>
+        /// <param name="portNumber">The port number.</param>
+        /// <returns></returns>
+        public static string GetMemoryMqConnectionString(string hostName, int portNumber = DefaultPorts.MemoryMq.NonSsl)
+        {
+            return GetConnectionString(DefaultSchemas.MemoryMq, hostName, portNumber);
+        }
+
+        /// <summary>
+        /// Gets the rabbit mq connection string.
+        /// </summary>
+        /// <param name="hostName">Name of the host.</param>
+        /// <param name="portNumber">The port number.</param>
+        /// <returns></returns>
+        public static string GetRabbitMqConnectionString(string hostName, int portNumber = DefaultPorts.RabbitMq.NonSsl)
+        {
+            return GetConnectionString(DefaultSchemas.RabbitMq, hostName, portNumber);
+        }
+
+        private static string GetConnectionString(string scheme, string hostName, int portNumber)
+        {
+            return string.Format("{0}://{1}:{2}", scheme, hostName, portNumber);
+        }
+    }
+}
---
+++ src/Thycotic.MessageQueue.Client/DefaultSchemas.cs
@@ -0,0 +1,18 @@
+﻿namespace Thycotic.MessageQueue.Client
+{
+    /// <summary>
+    /// Default message queue ports
+    /// </summary>
+    public static class DefaultSchemas
+    {
+        /// <summary>
+        /// The memory mq schema
+        /// </summary>
+        public const string MemoryMq = "net.tcp";
+
+        /// <summary>
+        /// The rabbit mq schema
+        /// </summary>
+        public const string RabbitMq = "amqp";
+    }
+}
