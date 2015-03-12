3803832848560a3a61225adcc408f41b88885f11
tpair24 <tpair24@thycotic.com>
2015-03-12 10:48:02 -0400

    DC  - Messages for password changing.

---
+++ src/Thycotic.Messages/PasswordChanging/Response/SecretChangePasswordResponse.cs
@@ -0,0 +1,42 @@
+﻿using Thycotic.AppCore.Federator;
+
+namespace Thycotic.Messages.PasswordChanging.Response
+{
+    /// <summary>
+    /// Secret heartbeat response
+    /// </summary>
+    public class SecretChangePasswordResponse
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="SecretChangePasswordResponse"/> is success.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if success; otherwise, <c>false</c>.
+        /// </value>
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// Gets or sets the error core.
+        /// </summary>
+        /// <value>
+        /// The error core.
+        /// </value>
+        public FailureCode ErrorCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the status messages.
+        /// </summary>
+        /// <value>
+        /// The status messages.
+        /// </value>
+        public string[] StatusMessages { get; set; }
+
+        /// <summary>
+        /// Gets or sets the command execution results.
+        /// </summary>
+        /// <value>
+        /// The command execution results.
+        /// </value>
+        public CommandExecutionResult[] CommandExecutionResults { get; set; }
+    }
+}
