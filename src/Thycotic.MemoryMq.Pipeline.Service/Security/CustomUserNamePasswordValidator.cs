cbc286e43616b0cb5c8307b243f64ae417cf4585
dobroslav.kolev@thycotic.com <dobroslav.kolev@thycotic.com>
2015-03-31 16:38:07 -0400

    Missing file

---
+++ src/Thycotic.MemoryMq.Pipeline.Service/Security/CustomUserNamePasswordValidator.cs
@@ -0,0 +1,23 @@
+﻿using System.IdentityModel.Selectors;
+using Thycotic.Logging;
+
+namespace Thycotic.MemoryMq.Pipeline.Service.Security
+{
+    /// <summary>
+    /// Custom username and password validator
+    /// </summary>
+    public class CustomUserNamePasswordValidator : UserNamePasswordValidator
+    {
+        private readonly ILogWriter _log = Log.Get(typeof(CustomUserNamePasswordValidator));
+
+        /// <summary>
+        /// When overridden in a derived class, validates the specified username and password.
+        /// </summary>
+        /// <param name="userName">The username to validate.</param>
+        /// <param name="password">The password to validate.</param>
+        public override void Validate(string userName, string password)
+        {
+            _log.Warn(string.Format("Accepting a pipeline client {0} without actually checking the credentials supplied", userName));
+        }
+    }
+}
