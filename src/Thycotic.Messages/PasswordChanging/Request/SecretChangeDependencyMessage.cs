d2df853f8bf87020715f4c5d1c6cd7ea3863e0c8
David Cooksey <david.cooksey@thycotic.com>
2015-04-02 11:38:33 -0400

    DC - Populated old and new password for RPC response. Package updates.

---
+++ src/Thycotic.Messages/PasswordChanging/Request/SecretChangeDependencyMessage.cs
@@ -0,0 +1,40 @@
+﻿using Thycotic.AppCore.Federator;
+using Thycotic.Messages.Common;
+
+namespace Thycotic.Messages.PasswordChanging.Request
+{
+    /// <summary>
+    /// Secret heartbeat message
+    /// </summary>
+    public class SecretChangeDependencyMessage : IBasicConsumable
+    {
+        /// <summary>
+        /// Gets or sets the password information provider.
+        /// </summary>
+        /// <value>
+        /// The password information provider.
+        /// </value>
+        public IDependencyChangerInfo PasswordInfoProvider { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Secret Id
+        /// </summary>
+        public int SecretId { get; set; }
+
+        /// <summary>
+        /// Gets the version.
+        /// </summary>
+        /// <value>
+        /// The version.
+        /// </value>
+        public int Version { get; set; }
+
+        /// <summary>
+        /// Gets or sets the retry count.
+        /// </summary>
+        /// <value>
+        /// The retry count.
+        /// </value>
+        public int RetryCount { get; set; }
+    }
+}
