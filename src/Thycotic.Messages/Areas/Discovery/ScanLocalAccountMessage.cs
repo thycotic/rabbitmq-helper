2192aa3cc74b39432649092d27c11f84580a0cab
thycopair70 <thycopair70@thycotic.com>
2015-04-02 14:28:01 -0400

    LLJRP - Stub out discovery

---
+++ src/Thycotic.Messages/Areas/Discovery/ScanLocalAccountMessage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thycotic.Messages.Common;
+
+namespace Thycotic.Messages.Areas.Discovery
+{
+    /// <summary>
+    /// Scan Local Account Message
+    /// </summary>
+    public class ScanLocalAccountMessage : IBasicConsumable
+    {
+
+        /// <summary>
+        /// Version
+        /// </summary>
+        public int Version { get; set; }
+
+
+        /// <summary>
+        /// Retry Count
+        /// </summary>
+        public int RetryCount { get; set; }
+    }
+}
---
+++ src/Thycotic.Messages/Areas/Discovery/ScanMachineMessage.cs
@@ -0,0 +1,21 @@
+﻿using Thycotic.Messages.Common;
+
+namespace Thycotic.Messages.Areas.Discovery
+{
+    /// <summary>
+    /// Scan Machine Message
+    /// </summary>
+    public class ScanMachineMessage : IBasicConsumable
+    {
+        /// <summary>
+        /// Version
+        /// </summary>
+        public int Version { get; set; }
+
+
+        /// <summary>
+        /// Retry Count
+        /// </summary>
+        public int RetryCount { get; set; }
+    }
+}
