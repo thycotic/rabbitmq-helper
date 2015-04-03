a4310b5403195272147425a3190aac5f3fe129e1
thycopair70 <thycopair70@thycotic.com>
2015-04-03 13:53:53 -0400

    LLJRP - Pass ComputerId on the Message

--- src/Thycotic.Messages/Areas/Discovery/Request/ScanDependencyMessage.cs
+++ src/Thycotic.Messages/Areas/Discovery/Request/ScanDependencyMessage.cs
@@ -8,6 +8,11 @@ namespace Thycotic.Messages.Areas.Discovery.Request
     public class ScanDependencyMessage : ScanMessageBase
     {
         /// <summary>
+        /// Computer Id
+        /// </summary>
+        public int ComputerId { get; set; }
+
+        /// <summary>
         /// Scan Computer Input
         /// </summary>
         public ScanComputerInput Input { get; set; }
--- src/Thycotic.Messages/Areas/Discovery/Request/ScanLocalAccountMessage.cs
+++ src/Thycotic.Messages/Areas/Discovery/Request/ScanLocalAccountMessage.cs
@@ -8,6 +8,12 @@ namespace Thycotic.Messages.Areas.Discovery.Request
     public class ScanLocalAccountMessage : ScanMessageBase
     {
         /// <summary>
+        /// Computer Id
+        /// </summary>
+        public int ComputerId { get; set; }
+
+
+        /// <summary>
         /// Scan Computer Input
         /// </summary>
         public ScanComputerInput Input { get; set; }
