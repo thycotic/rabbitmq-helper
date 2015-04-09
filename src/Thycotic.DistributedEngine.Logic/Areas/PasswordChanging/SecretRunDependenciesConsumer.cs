9c138af46bdb1720a9bebfff71f979b24bec088c
David Cooksey <david.cooksey@thycotic.com>
2015-04-09 15:57:19 -0400

    DC - #2268-SS - Added more logging to dependency changer and updated packages.

--- src/Thycotic.DistributedEngine.Logic/Areas/PasswordChanging/SecretRunDependenciesConsumer.cs
+++ src/Thycotic.DistributedEngine.Logic/Areas/PasswordChanging/SecretRunDependenciesConsumer.cs
@@ -9,6 +9,7 @@ using Thycotic.Logging;
 using Thycotic.MessageQueue.Client.QueueClient;
 using Thycotic.Messages.Common;
 using Thycotic.Messages.PasswordChanging.Request;
+using Thycotic.PasswordChangers;
 using Thycotic.PasswordChangers.DependencyChangers;
 using Thycotic.SharedTypes.Dependencies;
 
@@ -20,7 +21,6 @@ namespace Thycotic.DistributedEngine.Logic.Areas.PasswordChanging
     public class SecretRunDependenciesConsumer : IBasicConsumer<SecretChangeDependencyMessage>
     {
         private readonly IExchangeNameProvider _exchangeNameProvider;
-
         private readonly IResponseBus _responseBus;
         private readonly ILogWriter _log = Log.Get(typeof(SecretChangePasswordConsumer));
 
@@ -48,22 +48,24 @@ namespace Thycotic.DistributedEngine.Logic.Areas.PasswordChanging
             {
                 // TODO: Fix 30 second wire-up problem.  This needs to be part of the individual message as well?
                 var guid = Guid.NewGuid();
-                foreach (var info in request.DependencyChangeInfos)
+                for (int index = 0; index < request.DependencyChangeInfos.Length; index++)
                 {
+                    var info = request.DependencyChangeInfos[index];
                     var response = new DependencyChangeResponse();
                     var messages = new List<DependencyChangeResponseMessageToLocalize>();
                     response.SecretId = request.SecretId;
                     response.SecretDependencyId = info.SecretDependencyId;
                     response.TransactionGuid = guid;
 
-                    Thread.Sleep((info.WaitBeforeSeconds * 1000));
+                    Thread.Sleep(info.WaitBeforeSeconds*1000);
                     messages.Add(GetDependencyStartedLogEntry(info));
                     var result = new DependencyChangeDispatcher(30).ExecuteDependencyAction(info);
+                    messages.Add(GetDependencyFinishedLogEntry(info, result));
 
                     response.Success = result.Success;
                     response.StatusMessages = result.Errors.Select(s => s.DetailedMessage).ToArray();
                     response.LogMessages = messages.ToArray();
-
+                    response.Last = index == request.DependencyChangeInfos.Length - 1;
                     try
                     {
                         _responseBus.Execute(response);
@@ -90,6 +92,14 @@ namespace Thycotic.DistributedEngine.Logic.Areas.PasswordChanging
                     ? new DependencyChangeResponseMessageToLocalize {MessageName = "DependencyStartedRunningOnMachineOnExchange", Params = new object[] { info.ServiceName, info.MachineName, _exchangeNameProvider.GetCurrentExchange() }}
                     : new DependencyChangeResponseMessageToLocalize {MessageName = "DependencyStartedRunningOnExchange", Params = new object[] { info.ServiceName, _exchangeNameProvider.GetCurrentExchange() }};
            
+        }
+
+        private DependencyChangeResponseMessageToLocalize GetDependencyFinishedLogEntry(IDependencyChangeInfo info, OperationResult result)
+        {
+            var machineName = (info.MachineName ?? "").Trim();
+            return !string.IsNullOrEmpty(machineName)
+                ? new DependencyChangeResponseMessageToLocalize { Success = result.Success, MessageName = "SuccessfullyUpdatedDependencyOnMachine", Params = new object[] { info.ServiceName, machineName } }
+                : new DependencyChangeResponseMessageToLocalize {Success = result.Success, MessageName = result.Success ? "SuccessfullyUpdatedDependency" : "FailedToUpdateDependency", Params = new object[] {info.ServiceName}};
         }
     }
 }
