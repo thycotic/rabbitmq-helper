847829653ceed122637bd5047a884f1dfab34cba
dobroslav.kolev@thycotic.com <dobroslav.kolev@thycotic.com>
2015-04-13 20:17:23 -0400

    Wix standalone sane wip

---
+++ src/Thycotic.InstallerGenerator.InteractiveRunner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thycotic.InstallerGenerator.InteractiveRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
