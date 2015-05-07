119ab5480af921f842a61407f96bdac36ace7929
dobroslav.kolev@thycotic.com <dobroslav.kolev@thycotic.com>
2015-05-07 15:26:39 -0400

    Variable rename

--- src/Thycotic.InstallerGenerator/InstallerGeneratorWrapper.cs
+++ src/Thycotic.InstallerGenerator/InstallerGeneratorWrapper.cs
@@ -12,41 +12,41 @@ namespace Thycotic.InstallerGenerator
     {
         private readonly ILogWriter _log = Log.Get(typeof(InstallerGeneratorWrapper));
 
-        private void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        private void DirectoryCopy(string sourcePath, string destinationPath, bool recursive)
         {
-            _log.Debug(string.Format("Copying contents of {0} to {1}", sourceDirName, destDirName));
+            _log.Debug(string.Format("Copying contents of {0} to {1}", sourcePath, destinationPath));
 
-            // Get the subdirectories for the specified directory.
-            var dir = new DirectoryInfo(sourceDirName);
+            // get the subdirectories for the specified directory.
+            var dir = new DirectoryInfo(sourcePath);
             var dirs = dir.GetDirectories();
 
             if (!dir.Exists)
             {
                 throw new DirectoryNotFoundException(
                     "Source directory does not exist or could not be found: "
-                    + sourceDirName);
+                    + sourcePath);
             }
 
-            // If the destination directory doesn't exist, create it. 
-            if (!Directory.Exists(destDirName))
+            // if the destination directory doesn't exist, create it. 
+            if (!Directory.Exists(destinationPath))
             {
-                Directory.CreateDirectory(destDirName);
+                Directory.CreateDirectory(destinationPath);
             }
 
-            // Get the files in the directory and copy them to the new location.
+            // get the files in the directory and copy them to the new location.
             var files = dir.GetFiles();
             foreach (var file in files)
             {
-                var temppath = Path.Combine(destDirName, file.Name);
+                var temppath = Path.Combine(destinationPath, file.Name);
                 file.CopyTo(temppath, false);
             }
 
-            // If copying subdirectories, copy them and their contents to new location. 
-            if (!copySubDirs) return;
+            // if copying subdirectories, copy them and their contents to new location. 
+            if (!recursive) return;
 
             foreach (var subdir in dirs)
             {
-                var temppath = Path.Combine(destDirName, subdir.Name);
+                var temppath = Path.Combine(destinationPath, subdir.Name);
                 DirectoryCopy(subdir.FullName, temppath, true);
             }
         }
