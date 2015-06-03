5a546ce22a409392eb467e4a05658fc529fa00d9
David Cooksey <david.cooksey@thycotic.com>
2015-06-03 15:18:02 -0400

    DC - #2324-SS - Moved BasicBus to the Service project.  Created an EngineToServerConnectionManager to handle looking for the correct host endpoint when multiple are present.  Upgraded the EngineToServer version to include the new Ping() method.  Added EngineToServer to the Service tests project.  Routed the ConfigurationBus, PostAuthenticationBus, ResponseBus, and UpdateBus through the ConnectionManager.

---
+++ src/Thycotic.DistributedEngine.Service.Tests/EngineToServerConnectionManagerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using Thycotic.DistributedEngine.EngineToServerCommunication;
+using Thycotic.DistributedEngine.Service.EngineToServer;
+
+namespace Thycotic.DistributedEngine.Service.Tests
+{
+    [TestFixture]
+    public class EngineToServerConnectionManagerTests
+    {
+        [Test]
+        public void ShouldOpenValidSingleWebConnection()
+        {
+            var manager = new EngineToServerConnectionManager("http://localhost/ihawu", false);
+            var callback = Substitute.For<IEngineToServerCommunicationCallback>();
+            var service = manager.OpenLiveChannel(callback);
+            service.PreAuthenticate();
+        }
+
+        [Test]
+        public void ShouldFailToOpenInvalidSingleWebConnection()
+        {
+            try
+            {
+                var conn = new EngineToServerConnectionManager("http://invalid", false);
+                var callback = Substitute.For<IEngineToServerCommunicationCallback>();
+                var service = conn.OpenLiveChannel(callback);
+                service.Dispose();
+                Assert.Fail("Where is my exception?");
+            }
+            catch (EndpointNotFoundException)
+            {                
+            }
+        }
+    }
+}
---
+++ src/Thycotic.DistributedEngine.Service/EngineToServer/BasicBus.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Thycotic.DistributedEngine.EngineToServerCommunication;
+using Thycotic.DistributedEngine.Logic.Update;
+using Thycotic.Logging;
+
+namespace Thycotic.DistributedEngine.Service.EngineToServer
+{
+    /// <summary>
+    /// Basic bus
+    /// </summary>
+    public abstract class BasicBus
+    {
+        private readonly IEngineToServerConnectionManager _engineToServerConnectionManager;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="engineToServerConnectionManager"></param>
+        protected BasicBus(IEngineToServerConnectionManager engineToServerConnectionManager)
+        {
+            _engineToServerConnectionManager = engineToServerConnectionManager;
+        }
+
+        /// <summary>
+        /// Wraps the interaction.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="func"></param>
+        /// <param name="callback"></param>
+        /// <returns></returns>
+        protected T WrapInteraction<T>(Func<IEngineToServerCommunicationWcfService, T> func, IEngineToServerCommunicationCallback callback)
+        {
+            try
+            {
+                using (var channel = _engineToServerConnectionManager.OpenLiveChannel(callback))
+                {
+                    return func.Invoke(channel);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Bus broken down", ex);
+            }
+        }
+
+        /// <summary>
+        /// Wraps the interaction
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="callback"></param>
+        protected void WrapInteraction(Action<IEngineToServerCommunicationWcfService> action, IEngineToServerCommunicationCallback callback)
+        {
+            try
+            {
+                using (var channel = _engineToServerConnectionManager.OpenLiveChannel(callback))
+                {
+                    action.Invoke(channel);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Bus broken down", ex);
+            }
+        }
+
+        /// <summary>
+        /// Wraps the interaction
+        /// </summary>
+        /// <param name="action"></param>
+        protected void WrapInteraction(Action<IUpdateWebClient> action)
+        {
+            try
+            {
+                var channel = _engineToServerConnectionManager.OpenLiveUpdateWebClient();                
+                action.Invoke(channel);                
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Bus broken down", ex);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        protected T WrapInteraction<T>(Func<IUpdateWebClient, T> func)
+        {
+            try
+            {
+                var webClient = _engineToServerConnectionManager.OpenLiveUpdateWebClient();                
+                return func.Invoke(webClient);                
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Bus broken down", ex);
+            }
+        }
+
+        /// <summary>
+        /// Wraps the interaction.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="func">The function.</param>
+        /// <returns></returns>
+        /// <exception cref="System.ApplicationException">Bus broken down</exception>
+        protected static T WrapInteraction<T>(Func<T> func)
+        {
+            try
+            {
+                return func.Invoke();
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Bus broken down", ex);
+            }
+        }
+
+        /// <summary>
+        /// Wraps the interaction.
+        /// </summary>
+        /// <param name="action">The action.</param>
+        /// <exception cref="System.ApplicationException">Bus broken down</exception>
+        protected static void WrapInteraction(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Bus broken down", ex);
+            }
+        }
+    }
+}
