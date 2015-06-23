5363d8acf5449f38c86aef643af4ab77bb7d2357
David Cooksey <david.cooksey@thycotic.com>
2015-06-23 16:10:17 -0400

    DC - #2116-SS - Adding some basic coverage for Consumers.

---
+++ src/Thycotic.DistributedEngine.Logic.Tests/Areas/Discovery/DependencyConsumerTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using NSubstitute;
+using NUnit.Framework;
+using Thycotic.Discovery.Core.Inputs;
+using Thycotic.Discovery.Core.Results;
+using Thycotic.Discovery.Sources.Scanners;
+using Thycotic.DistributedEngine.EngineToServerCommunication.Areas.Discovery.Response;
+using Thycotic.DistributedEngine.Logic.Areas.Discovery;
+using Thycotic.DistributedEngine.Logic.EngineToServer;
+using Thycotic.Messages.Areas.Discovery.Request;
+
+namespace Thycotic.DistributedEngine.Logic.Tests.Areas.Discovery
+{
+    [TestFixture]
+    public class DependencyConsumerTests
+    {
+        [Test]
+        public void ShouldHandleErrorReturnWhenScanningForDependencies()
+        {
+            var responseBus = Substitute.For<IResponseBus>();
+            var scannerFactory = Substitute.For<IScannerFactory>();
+            var discoveryScanner = Substitute.For<IDiscoveryScanner>();
+            var consumer = new DependencyConsumer(responseBus, scannerFactory);
+
+            scannerFactory.GetDiscoveryScanner(1).Returns(discoveryScanner);
+            var scanInput = new ScanComputerInput {PageSize = 10};
+            discoveryScanner.ScanComputerForDependencies(scanInput).Returns(
+                new DependencyScanResult {ErrorCode = 1, ErrorMessage = "Error"});
+
+            var message = new ScanDependencyMessage();
+            message.DiscoveryScannerId = 1;
+            message.Input = scanInput;
+            consumer.Consume(message);
+
+            responseBus.Received().Execute(Arg.Any<ScanDependencyResponse>());
+        }
+
+        [Test]
+        public void ShouldNotPostMessageToServerWhenExceptionThrownByScanner()
+        {
+            var responseBus = Substitute.For<IResponseBus>();
+            var scannerFactory = Substitute.For<IScannerFactory>();
+            var discoveryScanner = Substitute.For<IDiscoveryScanner>();
+            var consumer = new DependencyConsumer(responseBus, scannerFactory);
+
+            scannerFactory.GetDiscoveryScanner(1).Returns(discoveryScanner);
+            var scanInput = new ScanComputerInput { PageSize = 10 };
+            discoveryScanner.ScanComputerForDependencies(scanInput).Returns(
+                x => { throw new Exception(); });
+
+            var message = new ScanDependencyMessage();
+            message.DiscoveryScannerId = 1;
+            message.Input = scanInput;
+            consumer.Consume(message);
+
+            responseBus.DidNotReceive().Execute(Arg.Any<ScanDependencyResponse>());
+        }
+    }
+}
---
+++ src/Thycotic.DistributedEngine.Logic.Tests/Areas/Heartbeat/SecretHeartbeatConsumerTests.cs
@@ -0,0 +1,28 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using Thycotic.DistributedEngine.EngineToServerCommunication.Areas.Heartbeat.Response;
+using Thycotic.DistributedEngine.Logic.Areas.Heartbeat;
+using Thycotic.DistributedEngine.Logic.EngineToServer;
+using Thycotic.Messages.Heartbeat.Request;
+using Thycotic.SharedTypes.PasswordChangers;
+
+namespace Thycotic.DistributedEngine.Logic.Tests.Areas.Heartbeat
+{
+    [TestFixture]
+    public class SecretHeartbeatConsumerTests
+    {
+        [Test]
+        public void ShouldHandleExceptionDueToNonMatchingVerifyCredentialsInfo()
+        {
+            var responseBus = Substitute.For<IResponseBus>();
+            var verifyCredentialsInfo = Substitute.For<IVerifyCredentialsInfo>();
+            var consumer = new SecretHeartbeatConsumer(responseBus);
+
+            var message = new SecretHeartbeatMessage();
+            message.VerifyCredentialsInfo = verifyCredentialsInfo;
+            consumer.Consume(message);
+
+            responseBus.Received().ExecuteAsync(Arg.Is<SecretHeartbeatResponse>(x => x.Status == OperationStatus.Unknown));
+        }
+    }
+}
