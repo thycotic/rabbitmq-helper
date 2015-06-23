06ccd4e1d7adf10afdd2899c9dde758df85a1f9a
David Cooksey <david.cooksey@thycotic.com>
2015-06-23 17:15:53 -0400

    DC - #2116-SS - Adding code coverage for Logic project.

---
+++ src/Thycotic.DistributedEngine.Logic.Tests/Areas/PasswordChanging/SecretBasicChangePasswordConsumerTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac.Features.OwnedInstances;
+using NSubstitute;
+using NUnit.Framework;
+using Thycotic.DistributedEngine.EngineToServerCommunication.Areas.PasswordChanging.Response;
+using Thycotic.DistributedEngine.Logic.Areas.PasswordChanging;
+using Thycotic.DistributedEngine.Logic.EngineToServer;
+using Thycotic.Messages.Common;
+using Thycotic.Messages.PasswordChanging.Request;
+using Thycotic.PasswordChangers.Windows;
+using Thycotic.SharedTypes.PasswordChangers;
+
+namespace Thycotic.DistributedEngine.Logic.Tests.Areas.PasswordChanging
+{
+    [TestFixture]
+    public class SecretBasicChangePasswordConsumerTests
+    {
+        [Test]
+        public void ShouldHandleExceptionDueToNonMatchingBasicInfo()
+        {
+            var responseBus = Substitute.For<IResponseBus>();
+            var info = Substitute.For<IBasicPasswordChangerInfo>();
+            var func = Substitute.For<Func<Owned<IBasicConsumer<SecretChangeDependencyMessage>>>>();
+
+            var consumer = new SecretBasicChangePasswordConsumer(responseBus, func);
+
+            var message = new SecretBasicPasswordChangeMessage();
+            message.OperationInfo = info;
+            consumer.Consume(message);
+
+            responseBus.Received().ExecuteAsync(Arg.Is<RemotePasswordChangeResponse>(x => x.Status == OperationStatus.Unknown));
+        }
+
+        [Test]
+        public void ShouldReturnErrorDueToInvalidLicense()
+        {
+            var responseBus = Substitute.For<IResponseBus>();
+            var info = new WindowsAccountBasicChangeInfo()
+            {
+                CurrentPassword = "invalidpassword",
+                EnsureTargetMachineHostAndReverseDnsRecordsMatch = false,
+                Machine = "1.1.1.1",
+                NewPassword = "new",
+                UserName = "user that does not exist"
+            };
+            var func = Substitute.For<Func<Owned<IBasicConsumer<SecretChangeDependencyMessage>>>>();
+
+            var consumer = new SecretBasicChangePasswordConsumer(responseBus, func);
+
+            var message = new SecretBasicPasswordChangeMessage();
+            message.OperationInfo = info;
+            consumer.Consume(message);
+
+            responseBus.Received().ExecuteAsync(Arg.Is<RemotePasswordChangeResponse>(x => x.Status == OperationStatus.Unknown && x.StatusMessages[0].Contains("Invalid License")));
+        }
+
+        [Test]
+        public void ShouldReturnErrorDueToIncorrectCredentials()
+        {
+            PasswordChangers.Aspects.LicenseKeyHelper.LicenseKeys = new Dictionary<string, string>
+            {
+                {"BFPJR-AB3JZ-NPWH5-U29TR-WMG14", "FOR DEVELOPMENT PURPOSES ONLY"}
+            };
+
+            var responseBus = Substitute.For<IResponseBus>();
+            var info = new WindowsAccountBasicChangeInfo
+            {
+                CurrentPassword = "invalidpassword",
+                EnsureTargetMachineHostAndReverseDnsRecordsMatch = false,
+                Machine = "thycopair23",
+                NewPassword = "new",
+                UserName = "user that does not exist"
+            };
+            var func = Substitute.For<Func<Owned<IBasicConsumer<SecretChangeDependencyMessage>>>>();
+
+            var consumer = new SecretBasicChangePasswordConsumer(responseBus, func);
+
+            var message = new SecretBasicPasswordChangeMessage();
+            message.OperationInfo = info;
+            consumer.Consume(message);
+
+            responseBus.Received().ExecuteAsync(Arg.Is<RemotePasswordChangeResponse>(x => x.Status == OperationStatus.AccessDenied));
+            PasswordChangers.Aspects.LicenseKeyHelper.LicenseKeys = null;
+        }
+    }
+}
---
+++ src/Thycotic.DistributedEngine.Logic.Tests/Areas/PasswordChanging/SecretRunDependencyConsumerTests.cs
@@ -0,0 +1,52 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using Thycotic.DistributedEngine.EngineToServerCommunication.Areas.Dependency.Response;
+using Thycotic.DistributedEngine.Logic.Areas.PasswordChanging;
+using Thycotic.DistributedEngine.Logic.EngineToServer;
+using Thycotic.MessageQueue.Client.QueueClient;
+using Thycotic.Messages.PasswordChanging.Request;
+using Thycotic.SharedTypes.Dependencies;
+
+namespace Thycotic.DistributedEngine.Logic.Tests.Areas.PasswordChanging
+{
+    [TestFixture]
+    public class SecretRunDependencyConsumerTests
+    {
+        [Test]
+        public void ShouldDoNothingWhenNoDependenciesPassedIn()
+        {
+            var responseBus = Substitute.For<IResponseBus>();
+            var nameProvider = Substitute.For<IExchangeNameProvider>();
+
+            var consumer = new SecretRunDependenciesConsumer(responseBus, nameProvider);
+
+            var message = new SecretChangeDependencyMessage();
+            message.DependencyChangeInfos = new IDependencyChangeInfo[0];
+            consumer.Consume(message);
+
+            responseBus.DidNotReceive().ExecuteAsync(Arg.Any<DependencyChangeResponse>());
+        }
+
+        [Test]
+        public void ShouldFailWithInvalidLicense()
+        {
+            var responseBus = Substitute.For<IResponseBus>();
+            var nameProvider = Substitute.For<IExchangeNameProvider>();
+
+            var consumer = new SecretRunDependenciesConsumer(responseBus, nameProvider);
+
+            var message = new SecretChangeDependencyMessage();
+            message.DependencyChangeInfos = new IDependencyChangeInfo[] {new ApplicationPoolChangeInfo
+            {
+                AccountDomainName = "testparent.thycotic.com",
+                AccountUserName = "username",
+                AccountPassword = "invalid password",
+                MachineName = "invalid machine"
+            }};
+            consumer.Consume(message);
+
+            responseBus.Received().ExecuteAsync(Arg.Is<DependencyChangeResponse>(x => !x.Success 
+                && x.StatusMessages[0].Contains("Invalid License")));
+        }        
+    }
+}
