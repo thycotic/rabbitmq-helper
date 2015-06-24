66b0f7c97d937f1961cff5360df67d10e34371e7
dobroslav.kolev@thycotic.com <dobroslav.kolev@thycotic.com>
2015-06-24 15:41:52 -0400

    DKLL - #SS-2476 - Missing files

---
+++ src/Thycotic.InstallerGenerator/Thycotic.InstallerGenerator.InteractiveRunner/ToolPaths.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace Thycotic.InstallerGenerator.InteractiveRunner
+{
+    /// <summary>
+    /// WiX tool paths
+    /// </summary>
+    public static class ToolPaths
+    {
+        private static readonly string LibPath = Path.Combine("lib");
+
+        /// <summary>
+        /// Gets the sign tool path.
+        /// </summary>
+        /// <param name="applicationPath">The application path.</param>
+        /// <returns></returns>
+        public static string GetSignToolPath(string applicationPath)
+        {
+            return Path.Combine(applicationPath, LibPath, "signtool.exe"); 
+        }
+    }
+}
