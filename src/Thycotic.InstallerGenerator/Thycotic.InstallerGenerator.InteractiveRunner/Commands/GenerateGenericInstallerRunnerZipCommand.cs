18f17911f1f6f4122ffdebc74f9b232090af3aad
dobroslav.kolev@thycotic.com <dobroslav.kolev@thycotic.com>
2015-06-26 12:57:21 -0400

    DK - #SS-2476 - Generic installer runner runbook and command WIP

---
+++ src/Thycotic.InstallerGenerator/Thycotic.InstallerGenerator.InteractiveRunner/Commands/GenerateGenericInstallerRunnerZipCommand.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Thycotic.CLI.Commands;
+using Thycotic.InstallerGenerator.Core;
+using Thycotic.InstallerGenerator.Core.WiX;
+using Thycotic.InstallerGenerator.Runbooks.Services;
+using Thycotic.Logging;
+
+namespace Thycotic.InstallerGenerator.InteractiveRunner.Commands
+{
+    class GenerateGenericInstallerRunnerZipCommand : CommandBase, IImmediateCommand
+    {
+        private readonly ILogWriter _log = Log.Get(typeof(GenerateGenericLegacyAgentServiceZipCommand));
+
+        public override string Area
+        {
+            get { return CommandAreas.Generic; }
+        }
+
+        public override string Description
+        {
+            get { return "Generates generic installer runner zip"; }
+        }
+
+        public GenerateGenericInstallerRunnerZipCommand()
+        {
+
+            Action = parameters =>
+            {
+                bool is32Bit;
+                parameters.TryGetBoolean("Is32Bit", out is32Bit);
+                var artifactPath = parameters["ArtifactPath"];
+                var binariesSourcePath = parameters["SourcePath.Binaries"];
+                
+                var installerVersion = parameters["Installer.Version"];
+
+                var steps = new GenericLegacyAgentServiceZipGeneratorRunbook
+                {
+                    Is64Bit = !is32Bit,
+                    ArtifactPath = artifactPath,
+                    SourcePath = binariesSourcePath,
+                    Version = installerVersion,
+                };
+                
+                var wrapper = new InstallerGeneratorWrapper();
+
+                var path = wrapper.Generate(new Generator(), steps);
+
+                return 0;
+
+            };
+        }
+    }
+}
---
+++ src/Thycotic.InstallerGenerator/Thycotic.InstallerGenerator.Runbooks.Services/GenericInstallerRunnerZipGeneratorRunbook.cs
@@ -0,0 +1,87 @@
+﻿using System.IO;
+using System.Text;
+using Thycotic.InstallerGenerator.Core;
+using Thycotic.InstallerGenerator.Core.Steps;
+
+namespace Thycotic.InstallerGenerator.Runbooks.Services
+{
+    /// <summary>
+    /// Distributed engine service WiX MSI generator runbook
+    /// </summary>
+    public class GenericInstallerRunnerZipGeneratorRunbook : InstallerGeneratorRunbook
+    {
+        /// <summary>
+        /// The default artifact name
+        /// </summary>
+        public const string DefaultArtifactName = "Thycotic.InstallerRunner";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenericLegacyAgentServiceZipGeneratorRunbook"/> class.
+        /// </summary>
+        public GenericInstallerRunnerZipGeneratorRunbook()
+        {
+            Is64Bit = false;
+        }
+
+        /// <summary>
+        /// Gets the name of the artifact file.
+        /// </summary>
+        /// <param name="artifactName">Name of the artifact.</param>
+        /// <param name="suffix">The suffix.</param>
+        /// <param name="is64Bit">if set to <c>true</c> [is64bit].</param>
+        /// <param name="version">The version.</param>
+        /// <returns></returns>
+        public static string GetArtifactFileName(string artifactName, string suffix, bool is64Bit, string version)
+        {
+            var sb = new StringBuilder();
+
+            sb.Append(artifactName);
+
+            if (!string.IsNullOrWhiteSpace(suffix))
+            {
+                sb.Append(string.Format("-{0}", suffix));
+            }
+
+            sb.Append(is64Bit ? "-x64" : "-x86");
+
+            sb.Append(string.Format(".{0}.zip", version));
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Bakes the steps.
+        /// </summary>
+        /// <exception cref="System.ArgumentException">Engine to server communication ingredients missing.</exception>
+        public override void BakeSteps()
+        {
+            ArtifactName = GetArtifactFileName(DefaultArtifactName, ArtifactNameSuffix, Is64Bit, Version);
+
+            Steps = new IInstallerGeneratorStep[]
+            {
+                new FileCopyStep
+                {
+                    SourcePath = ToolPaths.GetLegacyAgentBootstrapperPath(ApplicationPath),
+                    DestinationPath = Path.Combine(SourcePath, "SecretServerAgentBootstrap.exe")
+                },
+                new FileRenameStep
+                {
+                    SourcePath = Path.Combine(SourcePath, "Thycotic.DistributedEngine.Service.exe"),
+                    DestinationPath = Path.Combine(SourcePath, "SecretServerAgentService.exe")
+                },
+                new FileRenameStep
+                {
+                    SourcePath = Path.Combine(SourcePath, "Thycotic.DistributedEngine.Service.exe.config"),
+                    DestinationPath = Path.Combine(SourcePath, "SecretServerAgentService.exe.config")
+                },
+                //TODO: Copy old bootstrapper
+                new CreateZipStep
+                {
+                    Name = "File harvest (Zip)",
+                    SourcePath = SourcePath,
+                    ZipFilePath = Path.Combine(WorkingPath, ArtifactName)
+                }
+            };
+        }
+    }
+}
