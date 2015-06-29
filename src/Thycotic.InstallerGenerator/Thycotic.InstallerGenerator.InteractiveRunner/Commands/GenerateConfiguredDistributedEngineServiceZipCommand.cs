2d508162e8dca35bf3dd489be396ab5f12a3fbda
dobroslav.kolev@thycotic.com <dobroslav.kolev@thycotic.com>
2015-06-29 15:11:10 -0400

    DK - #SS-2476 - DE configured zip wip

---
+++ src/Thycotic.InstallerGenerator/Thycotic.InstallerGenerator.InteractiveRunner/Commands/GenerateConfiguredDistributedEngineServiceZipCommand.cs
@@ -0,0 +1,93 @@
+﻿using System.IO;
+using Thycotic.CLI.Commands;
+using Thycotic.InstallerGenerator.Core;
+using Thycotic.InstallerGenerator.Core.WiX;
+using Thycotic.InstallerGenerator.Runbooks.Services;
+using Thycotic.Logging;
+
+namespace Thycotic.InstallerGenerator.InteractiveRunner.Commands
+{
+    class GenerateConfiguredDistributedEngineServiceZipCommand : CommandBase, IImmediateCommand
+    {
+        private readonly ILogWriter _log = Log.Get(typeof(GenerateConfiguredDistributedEngineServiceZipCommand));
+
+        public override string Area
+        {
+            get { return CommandAreas.Generic; }
+        }
+
+        public override string Description
+        {
+            get { return "Generates a configured distributed engine MSI"; }
+        }
+
+        //public override string[] Examples
+        //{
+        //    get
+        //    {
+        //        return new[]
+        //        {
+        //            @"generateGenericMemoryMqSiteConnectorServiceMsi -ArtifactPath=C:\Users\dobroslav.kolev\Desktop\bits -SourcePath.Recipes=M:\development\repos\distributedengine\src\Thycotic.MemoryMq.SiteConnector.Service.Wix -SourcePath.Binaries=M:\development\repos\distributedengine\src\Thycotic.MemoryMq.SiteConnector.Service\bin\Release -Installer.Version=5.0.0.0 -Signing.PfxPath=C:\Users\dobroslav.kolev\Desktop\signing\SSDESPC.pfx -Signing.PfxPassword=password1"
+        //        };
+        //    }
+        //}
+
+        public GenerateConfiguredDistributedEngineServiceZipCommand()
+        {
+
+            Action = parameters =>
+            {
+                var connectionString = parameters["E2S.ConnectionString"];
+                bool useSsl;
+                if (!parameters.TryGetBoolean("E2S.UseSsl", out useSsl)) return -1;
+
+                string siteId;
+                if (!parameters.TryGet("E2S.Site", out siteId)) return -1;
+                string organizationId;
+                if (!parameters.TryGet("E2S.OrganizationID", out organizationId)) return -1;
+
+
+                bool is32Bit;
+                parameters.TryGetBoolean("Is32Bit", out is32Bit);
+                var artifactPath = parameters["ArtifactPath"];
+                string artifactName;
+                parameters.TryGet("ArtifactName", out artifactName);
+                var runnerZipPath = parameters["SourcePath.RunnerZip"];
+                var msiSourcePath = parameters["SourcePath.MSI"];
+
+                if (!File.Exists(msiSourcePath))
+                {
+                    throw new FileNotFoundException(string.Format("MSI could not be found at {0}", msiSourcePath));
+                }
+
+                var msiFileName = new FileInfo(msiSourcePath).Name;
+
+
+                var installerVersion = parameters["Installer.Version"];
+
+                var steps = new ConfiguredDistributedEngineServiceZipGeneratorRunbook
+                {
+                    Is64Bit = !is32Bit,
+                    ArtifactPath = artifactPath,
+                    ArtifactName = artifactName,
+                    MsiFileName = msiFileName,
+                    RunnerZipPath = runnerZipPath,
+                    SourcePath = msiSourcePath,
+                    Version = installerVersion,
+
+                    ConnectionString = connectionString,
+                    UseSsl = useSsl,
+                    SiteId = siteId,
+                    OrganizationId = organizationId
+                };
+
+                var wrapper = new InstallerGeneratorWrapper();
+
+                var path = wrapper.Generate(new BasicInstallerGenerator(), steps);
+
+                return 0;
+
+            };
+        }
+    }
+}
--- src/Thycotic.InstallerGenerator/Thycotic.InstallerGenerator.InteractiveRunner/Resources/generateAllInstallers.cmd
+++ src/Thycotic.InstallerGenerator/Thycotic.InstallerGenerator.InteractiveRunner/Resources/generateAllInstallers.cmd
@@ -15,6 +15,7 @@ echo Generating generic installers/updaters...
 echo 64Bit Distributed Engine
 %ig% generateGenericDistributedEngineServiceMsi -ArtifactName=gdesvc.msi -ArtifactPath=%artifactPath% -SourcePath.Recipes=%illidanMapPath%\src\Thycotic.DistributedEngine.Service.Wix -SourcePath.Binaries=%illidanMapPath%\src\Thycotic.DistributedEngine.Service\bin\Release -Installer.Version=5.0.0.0 -Signing.PfxPath=%pfxPath% -Signing.PfxPassword=%pfxPassword%
 if errorlevel 1 goto end
+
 echo 32Bit Distributed Engine
 %ig% generateGenericDistributedEngineServiceMsi -Is32Bit=true -ArtifactName=gdesvc.32Bit.msi -ArtifactPath=%artifactPath% -SourcePath.Recipes=%illidanMapPath%\src\Thycotic.DistributedEngine.Service.Wix.32Bit -SourcePath.Binaries=%illidanMapPath%\src\Thycotic.DistributedEngine.Service\bin\Release.32Bit -Installer.Version=5.0.0.0 -Signing.PfxPath=%pfxPath% -Signing.PfxPassword=%pfxPassword%
 if errorlevel 1 goto end
@@ -35,17 +36,26 @@ echo 64Bit Installer Runner
 %ig% generateGenericInstallerRunnerZip -ArtifactName=girapp.zip -ArtifactPath=%artifactPath% -SourcePath.Binaries=-SourcePath.Binaries=%illidanMapPath%\src\Thycotic.InstallerGenerator\Thycotic.InstallerRunner\bin\Release -Installer.Version=5.0.0.0 -Signing.PfxPath=%pfxPath% -Signing.PfxPassword=%pfxPassword%
 if errorlevel 1 goto end
 
+echo.
 echo NO SIGNING PAST THIS POINT
+echo.
 
 echo Generating configured installers/updaters...
 echo 64Bit Memory Mq site connector
 %ig% generateConfiguredMemoryMqSiteConnectorServiceZip -ArtifactName=cmmqsvc.zip  -ArtifactPath=%artifactPath% -SourcePath.MSI=%artifactPath%\gmmqsvc.msi -SourcePath.RunnerZip=%artifactPath%\girapp.zip -Installer.Version=5.0.0.0 -Pipeline.ConnectionString=net.tcp://illidan:8671 -Pipeline.UseSsl=false -Pipeline.Thumbprint=invalid
+
+echo 64Bit Distributed Engine
+%ig% generateConfiguredDistributedEngineServiceZip -ArtifactName=cdesvc.zip  -ArtifactPath=%artifactPath% -SourcePath.MSI=%artifactPath%\gdesvc.msi -SourcePath.RunnerZip=%artifactPath%\girapp.zip -Installer.Version=5.0.0.0 -E2S.ConnectionString=http://illidan/ihawu -E2S.UseSsl=false -E2S.SiteId=3 -E2S.OrganizationId=1
+
+echo 32Bit Distributed Engine
+%ig% generateConfiguredDistributedEngineServiceZip -Is32Bit=true -ArtifactName=cdesvc.32Bit.zip  -ArtifactPath=%artifactPath% -SourcePath.MSI=%artifactPath%\gdesvc.32Bit.msi -SourcePath.RunnerZip=%artifactPath%\girapp.zip -Installer.Version=5.0.0.0 -E2S.ConnectionString=http://illidan/ihawu -E2S.UseSsl=false -E2S.SiteId=3 -E2S.OrganizationId=1
+
 if errorlevel 1 goto end
 
 echo.
 echo Generation successful!
-:end
 
+:end
 if errorlevel 1 (
 	REM dark red
 	color 04
