a4642cd640d61bbbba2f12620e11a674f27f505e
dobroslav.kolev@thycotic.com <dobroslav.kolev@thycotic.com>
2015-07-09 17:02:28 -0400

    DK - SS-2371 - Commenting

--- src/Thycotic.Utility/Serialization/IObjectSerializer.cs
+++ src/Thycotic.Utility/Serialization/IObjectSerializer.cs
@@ -32,9 +32,18 @@ namespace Thycotic.Utility.Serialization
         byte[] ToBytes(object message);
     }
 
+    /// <summary>
+    /// Contract class
+    /// </summary>
     [ContractClassFor(typeof(IObjectSerializer))]
     public abstract class ObjectSerializerContract : IObjectSerializer
     {
+        /// <summary>
+        /// Turns the array of bytes into an object of the specified type.
+        /// </summary>
+        /// <typeparam name="TRequest">The type of the request.</typeparam>
+        /// <param name="bytes">The bytes.</param>
+        /// <returns></returns>
         public TRequest ToObject<TRequest>(byte[] bytes)
         {
             Contract.Requires<ArgumentNullException>(bytes != null);
@@ -42,6 +51,11 @@ namespace Thycotic.Utility.Serialization
             return default(TRequest);
         }
 
+        /// <summary>
+        /// Turns the array of bytes into an object.
+        /// </summary>
+        /// <param name="bytes">The bytes.</param>
+        /// <returns></returns>
         public object ToObject(byte[] bytes)
         {
             Contract.Requires<ArgumentNullException>(bytes != null);
@@ -49,6 +63,11 @@ namespace Thycotic.Utility.Serialization
             return default(object);
         }
 
+        /// <summary>
+        /// Turns the object into an array of bytes.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <returns></returns>
         public byte[] ToBytes(object message)
         {
             Contract.Requires<ArgumentNullException>(message != null);
