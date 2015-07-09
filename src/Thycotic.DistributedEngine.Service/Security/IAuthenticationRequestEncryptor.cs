503eea38ba16d6d7a780b08ba70ee65cbec36ed9
dobroslav.kolev@thycotic.com <dobroslav.kolev@thycotic.com>
2015-07-09 11:22:19 -0400

    DK - SS-2371 - Removing redundant contracts

--- src/Thycotic.DistributedEngine.Service/Security/IAuthenticationRequestEncryptor.cs
+++ src/Thycotic.DistributedEngine.Service/Security/IAuthenticationRequestEncryptor.cs
@@ -8,43 +8,7 @@ namespace Thycotic.DistributedEngine.Service.Security
     /// <summary>
     /// Authentication encryptor
     /// </summary>
-    [ContractClass(typeof(AuthenticationRequestEncryptorContract))]
     public interface IAuthenticationRequestEncryptor : IRequestEncryptor<PublicKey, PrivateKey>
     {
-    }
-    
-    /// <summary>
-    /// Contract for IAuthenticationRequestEncryptor
-    /// </summary>
-    [ContractClassFor(typeof(IAuthenticationRequestEncryptor))]
-    public abstract class AuthenticationRequestEncryptorContract : IAuthenticationRequestEncryptor
-    {
-        /// <summary>
-        /// Interface for an envelope Decrypter
-        /// </summary>
-        /// <param name="decryptionKey">Decryption Key</param>
-        /// <param name="bytes">Bytes</param>
-        /// <returns>Decrypted Byte Array</returns>
-        public byte[] Decrypt(PrivateKey decryptionKey, byte[] bytes)
-        {
-            Contract.Requires<ArgumentNullException>(decryptionKey!=null);
-            Contract.Requires<ArgumentNullException>(bytes!=null);
-
-            return default(byte[]);
-        }
-
-        /// <summary>
-        /// Interface for an envelope Encrypter
-        /// </summary>
-        /// <param name="encryptionKey">Encyption Key</param>
-        /// <param name="bytes">Bytes</param>
-        /// <returns>Encrypted Byte Array</returns>
-        public byte[] Encrypt(PublicKey encryptionKey, byte[] bytes)
-        {
-            Contract.Requires<ArgumentNullException>(encryptionKey != null);
-            Contract.Requires<ArgumentNullException>(bytes != null);
-
-            return default(byte[]);
-        }
     }
 }
