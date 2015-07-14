8360a2f6056040c848b03fd14299f5684ff2c35a
dobroslav.kolev@thycotic.com <dobroslav.kolev@thycotic.com>
2015-07-14 14:39:21 -0400

    DK - SS-2116 - Adding unit test required and user tests for

---
+++ src/Thycotic.Utility.Testing/TestChain/TestedSubstitute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NSubstitute;
+
+namespace Thycotic.Utility.Testing.TestChain
+{
+    public static class TestedSubstitute
+    {
+
+        /// <summary>
+        /// Generates substitute for a class or interface which requires tests.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="ApplicationException"></exception>
+        public static T For<T>()
+            where T : class
+        {
+            var type = typeof (T);
+
+            if (!type.IsDefined(typeof(UnitTestsRequiredAttribute), true))
+            {
+                throw new ApplicationException(
+                    string.Format(
+                        "The class or interface {0} you are trying to substitute for does not require that tests exist for it.",
+                        type.FullName));
+            }
+
+            return Substitute.For<T>();
+        }
+    }
+}
