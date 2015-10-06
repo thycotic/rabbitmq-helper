e7284225f6fc5826b25ecaae4469d4af8365aa6b
dcooksey <david.cooksey@thycotic.com>
2015-10-06 12:28:27 -0400

    DC - #2865-SS - Adding messages and consumer for authentication through Engine.  The message is generic enough that it can authenticate user1@domain2.com against domain3.com (the user's domain and the domain to authenticate against do not have to be the same).

---
+++ src/Thycotic.DistributedEngine.Logic/Areas/Authentication/AuthenticateByAdConsumer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.DirectoryServices;
+using Thycotic.Logging;
+using Thycotic.Messages.Authenticate.Request;
+using Thycotic.Messages.Authenticate.Response;
+using Thycotic.Messages.Common;
+
+namespace Thycotic.DistributedEngine.Logic.Areas.Authentication
+{
+    /// <summary>
+    /// Authenticate By AD Consumer
+    /// </summary>
+    public class AuthenticateByAdConsumer : IBlockingConsumer<AuthenticateByAdMessage, AuthenticateByAdResponse>
+    {
+        private readonly ILogWriter _log = Log.Get(typeof(AuthenticateByAdConsumer));
+
+        /// <summary>
+        /// Consumes the request
+        /// </summary>
+        /// <param name="request">AuthenticateByAdMessage</param>
+        /// <returns>AuthenticateByAdResponse</returns>
+        public AuthenticateByAdResponse Consume(AuthenticateByAdMessage request)
+        {
+            _log.Info(string.Format("Got an Authenticate-By-AD request for {0}@{1}", request.Username, request.UserDomain));
+
+            string domainandusername = request.Ldaps ? String.Concat(request.Username, "@", request.UserDomain) : String.Concat(request.UserDomain, "\\", request.Username);
+            string domainConnectionString = request.Port != 389 ? request.DomainToAuthenticateTo + ":" + request.Port : request.DomainToAuthenticateTo;
+            var authenticationType = request.Ldaps ? AuthenticationTypes.SecureSocketsLayer : AuthenticationTypes.Secure;
+            DirectoryEntry user = new DirectoryEntry("LDAP://" + domainConnectionString, domainandusername, request.Password, authenticationType);
+            try
+            {
+                user.RefreshCache(new[] { "objectClass" });
+            }
+            catch (Exception ex)
+            {
+                return new AuthenticateByAdResponse {Success = false, ErrorMessage = ex.ToString()};
+            }
+            finally
+            {
+                user.Dispose();
+            }
+            return new AuthenticateByAdResponse {Success = true};
+        }
+    }
+}
