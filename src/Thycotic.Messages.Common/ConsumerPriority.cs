b3a400c5e2bfc0d694fcdaf57f03ba6cbefe29d0
dcooksey <david.cooksey@thycotic.com>
2015-10-13 12:24:18 -0400

    DC - #2870-SS - Adding missed files.

---
+++ src/Thycotic.Messages.Common/ConsumerPriority.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Thycotic.Messages.Common
+{
+    /// <summary>
+    /// Attribute to specify a per-Consumer priority.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class ConsumerPriority : Attribute
+    {
+        /// <summary>
+        /// Priority
+        /// </summary>
+        public Priority Priority { get; set; }
+
+        /// <summary>
+        /// Creates a ConsumerPriority class
+        /// </summary>
+        /// <param name="priority"></param>
+        public ConsumerPriority(Priority priority)
+        {
+            Priority = priority;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum Priority
+    {
+        /// <summary>
+        /// BelowNormal
+        /// </summary>
+        BelowNormal = 1,
+
+        /// <summary>
+        /// Normal
+        /// </summary>
+        Normal = 2,
+
+        /// <summary>
+        /// AboveNormal
+        /// </summary>
+        AboveNormal = 3,
+        
+        /// <summary>
+        /// Highest
+        /// </summary>
+        Highest = 4
+    }
+}
