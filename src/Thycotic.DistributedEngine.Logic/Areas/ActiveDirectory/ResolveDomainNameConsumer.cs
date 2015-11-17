210f1b0b0adadc2413d05387fb53b8f1fcccc564
John Allers <john.allers@gmail.com>
2015-11-17 13:01:52 -0500

    JA - #3008-SS - Adding consumers to handle requests for resolving a domain's FQDN or distinguished name.

---
+++ src/Thycotic.DistributedEngine.Logic/Areas/ActiveDirectory/ResolveDomainNameConsumer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Thycotic.ActiveDirectory;
+using Thycotic.ActiveDirectory.Core;
+using Thycotic.Logging;
+using Thycotic.Messages.Areas.ActiveDirectory;
+using Thycotic.Messages.Areas.ActiveDirectory.Response;
+using Thycotic.Messages.Common;
+
+namespace Thycotic.DistributedEngine.Logic.Areas.ActiveDirectory
+{
+    /// <summary>
+    /// Consumer for handling requests to resolve various domain name formats.
+    /// </summary>
+    public class ResolveDomainNameConsumer :
+        IBlockingConsumer<ResolveDomainDistinguishedNameMessage, DomainDistinguishedNameResponse>,
+        IBlockingConsumer<ResolveFullyQualifiedDomainNameMessage, FullyQualifiedDomainNameResponse>
+    {
+        private readonly ILogWriter _log = Log.Get(typeof(ResolveDomainNameConsumer));
+
+        /// <summary>
+        /// Consumes a ResolveDomainDistinguishedNameMessage.
+        /// </summary>
+        /// <param name="request">A ResolveDomainDistinguishedNameMessage.</param>
+        /// <returns>A DomainDistinguishedNameResponse.</returns>
+        public DomainDistinguishedNameResponse Consume(ResolveDomainDistinguishedNameMessage request)
+        {
+            try
+            {
+                _log.Info(string.Format("Resolving distinguished name for domain: {0}", request.DomainName));
+                var connectionSettings = new ConnectionSettings
+                {
+                    SecureSocketLayer = request.UseSSL
+                };
+                
+                var distinguishedName = DistinguishedNameHelper.ResolveDomainDistinguishedName(request.DomainName, request.UserPrincipalName, request.UserPassword, connectionSettings);
+                _log.Debug("Resolve domain to " + (distinguishedName ?? "(null)") );
+                
+                return new DomainDistinguishedNameResponse
+                {
+                    DomainDistinguishedName = distinguishedName
+                };
+            }
+            catch (Exception e)
+            {
+                _log.Error("Domain DN resolution failed" + ": ", e);
+                return new DomainDistinguishedNameResponse
+                {
+                    ErrorMessage = e.Message
+                };
+            }       
+        }
+
+        /// <summary>
+        /// Consumes a ResolveFullyQualifiedDomainNameMessage.
+        /// </summary>
+        /// <param name="request">A ResolveFullyQualifiedDomainNameMessage.</param>
+        /// <returns>A FullyQualifiedDomainNameResponse.</returns>
+        public FullyQualifiedDomainNameResponse Consume(ResolveFullyQualifiedDomainNameMessage request)
+        {
+            try
+            {
+                _log.Info(string.Format("Resolving fully qualified name for domain: {0}", request.FriendlyDomainName));
+                var connectionSettings = new ConnectionSettings
+                {
+                    SecureSocketLayer = request.UseSSL
+                };
+
+                var fqdn = DistinguishedNameHelper.ResolveDomainDistinguishedName(request.FriendlyDomainName, request.UserPrincipalName, request.UserPassword, connectionSettings);
+                _log.Debug("Resolve domain to " + (fqdn ?? "(null)"));
+
+                return new FullyQualifiedDomainNameResponse
+                {
+                    FullyQualifiedDomainName = fqdn
+                };
+            }
+            catch (Exception e)
+            {
+                _log.Error("FQDN resolution failed" + ": ", e);
+                return new FullyQualifiedDomainNameResponse
+                {
+                    ErrorMessage = e.Message
+                };
+            }
+        }
+    }
+}
