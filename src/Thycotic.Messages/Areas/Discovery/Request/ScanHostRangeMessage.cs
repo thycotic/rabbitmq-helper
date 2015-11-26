6b4deea9793e88d3f67a7e409cb212653711ef4c
thycopair23 <thycopair23@thycotic.com>
2015-11-26 12:29:59 -0500

    ZL - #2988-SS - Adding IBlockingConsumable to ScanHostRangeMessage so that I can make a blocking call to it from SS.

--- src/Thycotic.Messages/Areas/Discovery/Request/ScanHostRangeMessage.cs
+++ src/Thycotic.Messages/Areas/Discovery/Request/ScanHostRangeMessage.cs
@@ -1,11 +1,12 @@
 ﻿using Thycotic.Discovery.Core.Inputs;
+using Thycotic.Messages.Common;
 
 namespace Thycotic.Messages.Areas.Discovery.Request
 {
     /// <summary>
     /// Scan Host Range Message
     /// </summary>
-    public class ScanHostRangeMessage : ScanMessageBase
+    public class ScanHostRangeMessage : ScanMessageBase, IBlockingConsumable
     {
         /// <summary>
         /// Scan Host Range Input
