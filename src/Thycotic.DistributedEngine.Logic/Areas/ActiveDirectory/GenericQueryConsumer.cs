b6c478eba3ed7ffa5e971ba722c239bce94c0420
John Allers <john.allers@gmail.com>
2015-12-11 11:17:05 -0500

    JA - #3130-SS - Moved logic for querying all users on a domain to a basic consumer that can page results. The remaining consumers in the GenericQueryConsumer are now limited to 300 items, to prevent issues with the WCF message size.

--- src/Thycotic.DistributedEngine.Logic/Areas/ActiveDirectory/GenericQueryConsumer.cs
+++ src/Thycotic.DistributedEngine.Logic/Areas/ActiveDirectory/GenericQueryConsumer.cs
@@ -6,56 +6,25 @@ using Thycotic.ActiveDirectory.Core;
 using Thycotic.Logging;
 using Thycotic.Messages.Areas.ActiveDirectory;
 using Thycotic.Messages.Common;
-using DomainInfo = Thycotic.ActiveDirectory.Core.DomainInfo;
 
 namespace Thycotic.DistributedEngine.Logic.Areas.ActiveDirectory
 {
     /// <summary>
     /// Consumer for searching AD objects.
     /// </summary>
-    public class GenericQueryConsumer : 
-        IBlockingConsumer<AllUsersByDomainQueryMessage, ADObjectsQueryResult>,
+    public class GenericQueryConsumer :
         IBlockingConsumer<GroupsByDomainQueryMessage, ADObjectsQueryResult>,
         IBlockingConsumer<UsersByGroupsQueryMessage, ADObjectsQueryResult>
     {
+        /* 
+         * JA - We need to restrict the amount of data returned to prevent issues with WCF message sizes.
+         * Ideally, the block calls should ask for specific pages of data. Unfortunately, at this time, the
+         * underlying logic for querying AD is very inefficient (queries everything, rather than just a single
+         * page), making paged calls to the engine even more inefficient.
+         */
+        private const int WCF_MAX_RESULT_SIZE = 300;
+
         private readonly ILogWriter _log = Log.Get(typeof(GenericQueryConsumer));
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="request"></param>
-        /// <returns></returns>
-        public ADObjectsQueryResult Consume(AllUsersByDomainQueryMessage request)
-        {
-            const int maxSize = 300;
-            try
-            {
-                _log.Info(string.Format("{0} : Searching for all users.", string.Join(", ", request.DomainInfo.DomainName)));
-
-                var results = new ActiveDirectorySearcher().GetAllUserADObjectsInActiveDirectory(ConvertRequestToActiveDirectoryDomainInfo(request));
-
-                var totalUsers = results.ADObjects.Count;
-                _log.Info(String.Format("{0} users found.", totalUsers));
-
-                if (totalUsers > maxSize)
-                {
-                    _log.Info(String.Format("There more than {0} users. Results will be truncated.", maxSize));
-                    return new ADObjectsQueryResult
-                    {
-                        ADObjects = results.ADObjects.OrderBy(o => o.Name).Take(maxSize).ToList(),
-                        Error = results.Error
-                    };
-                }
-
-                return results;
-            }
-            catch (Exception e)
-            {
-                const string error = "Search for all users failed";
-                _log.Error(error + ": ", e);
-                return new ADObjectsQueryResult(new List<ADObject>(), error);
-            }
-        }
 
         /// <summary>
         /// 
@@ -71,12 +40,27 @@ namespace Thycotic.DistributedEngine.Logic.Areas.ActiveDirectory
                 var input = new SearchActiveDirectoryInput
                 {
                     BatchSize = request.BatchSize,
-                    Domain = ConvertRequestToActiveDirectoryDomainInfo(request),
+                    Domain = request.DomainInfo.ToCoreDomainInfo(),
                     Filter = request.Filter,
                     NamesToExclude = request.NamesToExclude
                 };
 
-                return new ActiveDirectorySearcher().SearchForGroupADObjectsInActiveDirectory(input);
+                var results = new ActiveDirectorySearcher().SearchForGroupADObjectsInActiveDirectory(input);
+
+                var totalGroups = results.ADObjects.Count;
+                _log.Info(String.Format("{0} groups found.", totalGroups));
+
+                if (totalGroups > WCF_MAX_RESULT_SIZE)
+                {
+                    _log.Info(String.Format("There more than {0} groups. Results will be truncated.", WCF_MAX_RESULT_SIZE));
+                    return new ADObjectsQueryResult
+                    {
+                        ADObjects = results.ADObjects.OrderBy(o => o.Name).Take(WCF_MAX_RESULT_SIZE).ToList(),
+                        Error = results.Error
+                    };
+                }
+
+                return results;
             }
             catch (Exception e)
             {
@@ -100,12 +84,27 @@ namespace Thycotic.DistributedEngine.Logic.Areas.ActiveDirectory
                 var input = new SearchActiveDirectoryInput
                 {
                     BatchSize = request.BatchSize,
-                    Domain = ConvertRequestToActiveDirectoryDomainInfo(request),
+                    Domain = request.DomainInfo.ToCoreDomainInfo(),
                     Filter = request.Filter,
                     NamesToExclude = request.NamesToExclude
                 };
 
-                return new ActiveDirectorySearcher().GetUserADObjectsForGroupInActiveDirectory(input);
+                var results = new ActiveDirectorySearcher().GetUserADObjectsForGroupInActiveDirectory(input);
+
+                var totalGroups = results.ADObjects.Count;
+                _log.Info(String.Format("{0} users found.", totalGroups));
+
+                if (totalGroups > WCF_MAX_RESULT_SIZE)
+                {
+                    _log.Info(String.Format("There more than {0} users. Results will be truncated.", WCF_MAX_RESULT_SIZE));
+                    return new ADObjectsQueryResult
+                    {
+                        ADObjects = results.ADObjects.OrderBy(o => o.Name).Take(WCF_MAX_RESULT_SIZE).ToList(),
+                        Error = results.Error
+                    };
+                }
+
+                return results;
             }
             catch (Exception e)
             {
@@ -113,21 +112,6 @@ namespace Thycotic.DistributedEngine.Logic.Areas.ActiveDirectory
                 _log.Error(error + ": ", e);
                 return new ADObjectsQueryResult(new List<ADObject>(), error);
             }
-        }
-
-        private static DomainInfo ConvertRequestToActiveDirectoryDomainInfo(QueryMessage request)
-        {
-            return new DomainInfo
-            {
-                DistinguishedName = request.DomainInfo.DistinguishedName,
-                DomainName = request.DomainInfo.DomainName,
-                Password = request.DomainInfo.Password,
-                ProtocolVersion = request.DomainInfo.ProtocolVersion,
-                UserName = request.DomainInfo.UserName,
-                Port = request.DomainInfo.Port,
-                LdapTimeoutInSeconds = request.DomainInfo.LdapTimeoutInSeconds,
-                UseSecureLdap = request.DomainInfo.UseSecureLdap
-            };
         }
     }
 }
