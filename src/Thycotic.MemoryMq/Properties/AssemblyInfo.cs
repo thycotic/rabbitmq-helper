2f6712def906b894b644daeb02da7f0133340476
dobroslav.kolev@thycotic.com <dobroslav.kolev@thycotic.com>
2015-12-14 12:55:42 -0500

    DK - #3142-SS Fixing assembly info

--- src/Thycotic.MemoryMq/Properties/AssemblyInfo.cs
+++ src/Thycotic.MemoryMq/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Thycotic.MemoryMqClient")]
+[assembly: AssemblyTitle("Thycotic.MemoryMq")]
 [assembly: AssemblyDescription("")]
 
 [assembly: AssemblyConfiguration("")]
