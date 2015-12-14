602b2e079135c4bd74f6dfc1feb14d94ba237a6a
dobroslav.kolev@thycotic.com <dobroslav.kolev@thycotic.com>
2015-12-14 10:32:16 -0500

    DK - #3147-SS Move RaquestBus factory to MessageQueue Client

--- src/Thycotic.MessageQueue.Client/RequestBusFactory.cs
+++ src/Thycotic.MessageQueue.Client/RequestBusFactory.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Diagnostics.Contracts;
-using Thycotic.MessageQueue.Client;
 using Thycotic.MessageQueue.Client.QueueClient;
+using Thycotic.MessageQueue.Client.QueueClient.AzureServiceBus;
 using Thycotic.MessageQueue.Client.QueueClient.MemoryMq;
 using Thycotic.MessageQueue.Client.QueueClient.RabbitMq;
 using Thycotic.Utility.Serialization;
 
-namespace Thycotic.DistributedEngine.Client
+namespace Thycotic.MessageQueue.Client
 {
     /// <summary>
     /// Request bus factory
@@ -59,6 +59,31 @@ namespace Thycotic.DistributedEngine.Client
             Contract.Ensures(Contract.Result<IRequestBus>() != null);
 
             connection = new MemoryMqConnection(url, userName, password, useSsl);
+
+            var objectSerializer = new JsonObjectSerializer();
+
+            return new RequestBus(connection, objectSerializer, messageEncryptor);
+        }
+
+        /// <summary>
+        /// Gets the Azure Service bus.
+        /// </summary>
+        /// <param name="url">The URL.</param>
+        /// <param name="sharedAccessKeyName">Name of the shared access key.</param>
+        /// <param name="sharedAccessKeyValue">The shared access key value.</param>
+        /// <param name="messageEncryptor">The message encryptor.</param>
+        /// <param name="connection">The connection for the bus which should be disposed after the bus is no longer needed.</param>
+        /// <returns></returns>
+        public static IRequestBus GetAzureServiceBus(string url, string sharedAccessKeyName, string sharedAccessKeyValue, IMessageEncryptor messageEncryptor, out ICommonConnection connection)
+        {
+            Contract.Requires<ArgumentNullException>(!string.IsNullOrWhiteSpace(url));
+            Contract.Requires<ArgumentNullException>(!string.IsNullOrWhiteSpace(sharedAccessKeyName));
+            Contract.Requires<ArgumentNullException>(!string.IsNullOrWhiteSpace(sharedAccessKeyValue));
+            Contract.Requires<ArgumentNullException>(messageEncryptor != null);
+
+            Contract.Ensures(Contract.Result<IRequestBus>() != null);
+
+            connection = new AzureServiceBusConnection(url, sharedAccessKeyName, sharedAccessKeyValue);
 
             var objectSerializer = new JsonObjectSerializer();
 
