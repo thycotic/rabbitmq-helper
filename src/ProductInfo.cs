8ef257c53f2e0569a5b782bb2c323fd32382110f
dobroslav.kolev@thycotic.com <dobroslav.kolev@thycotic.com>
2015-12-14 13:49:09 -0500

    DK - #3142-SS - Removing usages of ProjectInfo.cs

--- src/ProductInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Thycotic Software Ltd.")]
-[assembly: AssemblyProduct("Secret Server")]
-[assembly: AssemblyCopyright("Copyright © Thycotic Software Ltd. 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
