a51b820ec1ab3ee13c34d8a0e60825bb6e223e29
dobroslav.kolev@thycotic.com <dobroslav.kolev@thycotic.com>
2016-04-08 08:54:26 -0400

    DK - Logging every consumer that will be registered

--- src/Thycotic.DistributedEngine.Service/IoC/LogicModule.cs
+++ src/Thycotic.DistributedEngine.Service/IoC/LogicModule.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 using System.Threading;
 using Autofac;
@@ -42,15 +43,24 @@ namespace Thycotic.DistributedEngine.Service.IoC
 
             _log.Debug("Initializing consumers...");
 
-            LoadConsumers(builder, typeof (IBasicConsumer<>));
-            LoadConsumers(builder, typeof (IBlockingConsumer<,>));
+            LoadConsumers(builder, typeof(IBasicConsumer<>));
+            LoadConsumers(builder, typeof(IBlockingConsumer<,>));
         }
 
         private void LoadConsumers(ContainerBuilder builder, Type type)
         {
-            var logicAssembly = Assembly.GetAssembly(typeof (HelloWorldConsumer));
+            var logicAssembly = Assembly.GetAssembly(typeof(HelloWorldConsumer));
 
             _log.Debug(string.Format("Registering consumers of type {0}", type));
+
+            var eligibleConsumerTypes = logicAssembly.GetTypes()
+                .Where(t => t.IsAssignableToGenericType(type))
+#if !DEBUG
+                .Where(t => !typeof(IRegisterForPocOnly).IsAssignableFrom(t)) //disabled POC functionality
+#endif
+                .ToList();
+
+            eligibleConsumerTypes.ForEach(t => _log.Debug(string.Format("Registering consumer type {0}", t)));
 
             builder.RegisterAssemblyTypes(logicAssembly)
                 .Where(t => t.IsAssignableToGenericType(type))
