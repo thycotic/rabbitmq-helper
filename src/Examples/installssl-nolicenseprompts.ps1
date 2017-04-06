be02213e7093ea7ba68bb4f7b2b3d19ea87a4a5d
dobroslav.kolev@thycotic.com <dobroslav.kolev@thycotic.com>
2017-04-06 11:13:12 -0400

    DK - Example script fixes.

--- src/Examples/installssl-nolicenseprompts.ps1
+++ src/Examples/installssl-nolicenseprompts.ps1
@@ -2,4 +2,4 @@
 import-module $PSScriptRoot\..\Thycotic.RabbitMq.Helper.PSCommands\bin\Release\Thycotic.RabbitMq.Helper.PSCommands.dll
 
 $path = "M:\development\vso\DistributedEngine\Thycotic.RabbitMq.Helper\src\Examples"
-install-Connector -verbose -hostname THYCOPAIR23.testparent.thycotic.com -useSsl -rabbitMqUsername SITEUN1 -rabbitMqPw SITEPW1 -cacertpath $path\sc.cer -pfxPath $path\sc.pfx -pfxPw password1 -agreeErlangLicense -agreeRabbitMqLicense
+install-Connector -verbose -hostname localhost -useSsl -rabbitMqUsername SITEUN1 -rabbitMqPw SITEPW1 -cacertpath $path\sc.cer -pfxPath $path\sc.pfx -pfxPw password1 -agreeErlangLicense -agreeRabbitMqLicense
