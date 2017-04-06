71217544722a9fee099dd6c32945595e99904b61
dobroslav.kolev@thycotic.com <dobroslav.kolev@thycotic.com>
2017-04-06 12:55:09 -0400

    DK - Powershell examples

--- src/Examples/installssl-nolicenseprompts.ps1
+++ src/Examples/installssl-nolicenseprompts.ps1
@@ -1,5 +1,5 @@
 #Please replace the sc.cer and sc.pfx files with your own certificate files
 import-module $PSScriptRoot\..\Thycotic.RabbitMq.Helper.PSCommands\bin\Release\Thycotic.RabbitMq.Helper.PSCommands.dll
 
-$path = "M:\development\vso\DistributedEngine\Thycotic.RabbitMq.Helper\src\Examples"
+$path = "$PSScriptRoot"
 install-Connector -verbose -hostname localhost -useSsl -rabbitMqUsername SITEUN1 -rabbitMqPw SITEPW1 -cacertpath $path\sc.cer -pfxPath $path\sc.pfx -pfxPw password1 -agreeErlangLicense -agreeRabbitMqLicense
