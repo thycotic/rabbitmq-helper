e567657936fdd37e886d288deb85a8e2e3cba5d7
Michael White <michael.white@thycotic.com>
2017-04-10 13:27:45 -0400

    MW #SS-5427 Tweaks to example scripts

--- src/Examples/installssl-nolicenseprompts.ps1
+++ src/Examples/installssl-nolicenseprompts.ps1
@@ -2,4 +2,4 @@
 import-module $PSScriptRoot\..\Thycotic.RabbitMq.Helper.PSCommands\bin\Release\Thycotic.RabbitMq.Helper.PSCommands.dll
 
 $path = "$PSScriptRoot"
-install-Connector -verbose -hostname localhost -useSsl -rabbitMqUsername SITEUN1 -rabbitMqPw SITEPW1 -cacertpath $path\sc.cer -pfxPath $path\sc.pfx -pfxPw password1 -agreeErlangLicense -agreeRabbitMqLicense
+install-Connector -verbose -Hostname localhost -useSsl -rabbitMqUsername SITEUN1 -rabbitMqPw SITEPW1 -cacertpath $path\sc.cer -pfxPath $path\sc.pfx -pfxPw password1 -agreeErlangLicense -agreeRabbitMqLicense
