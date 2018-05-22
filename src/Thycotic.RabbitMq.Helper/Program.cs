57083be476742d367de8447e37d9b3e263a03f9d
Dobroslav Kolev <dkolev@compfreak.com>
2018-05-22 12:20:30 -0400

    Handling old versions of powershell.

--- src/Thycotic.RabbitMq.Helper/Program.cs
+++ src/Thycotic.RabbitMq.Helper/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Diagnostics;using System.IO;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
@@ -12,47 +13,94 @@ namespace Thycotic.RabbitMq.Helper
     {
         private static void Main(string[] args)
         {
-            Console.Write("Starting PowerShell...");
-
-            var task = Task.Run(() =>
+            try
             {
-                var module = typeof(InstallConnectorCommand).Assembly;
+                Console.WriteLine("Preparing helper and starting PowerShell...");
 
-                var exampleFolder = ".\\Examples";
-                var preparationScript = $"Write-Host \"Running RabbitMq Helper version {module.GetName().Version}\"; " +
-                                        $"Write-Host \"Execution Policy:\";" +
-                                        $"Get-ExecutionPolicy | Write-Host;" +
-                                        $"Write-Host;" +
-
-                                        $"Import-Module \"{module.Location}\"; " +
-
-                                        $"Write-Host \"You can use the provided Example PowerShell scripts or invoke any of the available command-lets directly.\";" +
-                                        $"Write-Host;" +
-
-                                        $"Write-Host \"Available scripts in {exampleFolder}:\";" +
-                                        $"Get-ChildItem -Filter *.ps1 -Path {exampleFolder} -Recurse -File | % {{ Write-Host \"`t {exampleFolder}\\$_\" }};" +
-                                        $"Write-Host;" +
-                                        
-                                        $"Write-Host \"Available command-lets:\";" +
-                                        $"Get-Command -Module {module.GetName().Name} | % {{ Write-Host \"`t $_.Name\" }};" +
-                                        $"Write-Host;";
-
-                var psi = new ProcessStartInfo
+                var task = Task.Run(() =>
                 {
-                    FileName = "powershell.exe",
-                    Arguments = $" -NoExit -ExecutionPolicy RemoteSigned & {{& {preparationScript} }}"
+                    var module = typeof(InstallConnectorCommand).Assembly;
 
-                };
-                var process = new Process {StartInfo = psi};
-                process.Start();
+                    var psi = new ProcessStartInfo
+                    {
+                        FileName = "powershell.exe",
+                        Arguments = $" -Version 3.0 -NoExit"
 
-                process.WaitForExit();
+                    };
+                    var process = new Process { StartInfo = psi };
+                    process.Start();
 
-            });
+                    process.WaitForExit();
 
-            Console.Write("done.");
+                    if (process.ExitCode != 0)
+                    {
+                        throw new ApplicationFailedException($"PowerShell version check failed. Process existed with code {process.ExitCode}. RabbitMq Helper requires PowerShell version 3 or higher. Please verify your installation.");
+                    }
 
-            task.Wait();
+                    var exampleFolder = ".\\Examples";
+                    var preparationScript = $"Write-Host \"Running RabbitMq Helper version {module.GetName().Version}\"; " +
+                                            $"Write-Host \"Execution Policy:\";" +
+                                            $"Get-ExecutionPolicy | Write-Host;" +
+                                            $"Write-Host;" +
+
+                                            $"Import-Module \"{module.Location}\"; " +
+
+                                            $"Write-Host \"You can use the provided Example PowerShell scripts or invoke any of the available command-lets directly.\";" +
+                                            $"Write-Host;" +
+
+                                            $"Write-Host \"Available scripts in {exampleFolder}:\";" +
+                                            $"Get-ChildItem -Filter *.ps1 -Path {exampleFolder} -Recurse -File | % {{ Write-Host \"`t {exampleFolder}\\$_\" }};" +
+                                            $"Write-Host;" +
+
+                                            $"Write-Host \"Available command-lets:\";" +
+                                            $"Get-Command -Module {module.GetName().Name} | % {{ Write-Host \"`t $_.Name\" }};" +
+                                            $"Write-Host;";
+
+                    psi = new ProcessStartInfo
+                    {
+                        FileName = "powershell.exe",
+                        Arguments = $" -Version 3.0 -NoExit -ExecutionPolicy RemoteSigned & {{& {preparationScript} }}"
+
+                    };
+                    process = new Process { StartInfo = psi };
+                    process.Start();
+
+                    process.WaitForExit();
+
+                    if (process.ExitCode != 0)
+                    {
+                        throw new ApplicationFailedException($"PowerShell existed with code {process.ExitCode}");
+                    }
+
+                });
+
+                task.Wait();
+
+                if (task.Exception != null)
+                {
+                    throw task.Exception;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error occured:");
+
+                var ex2 = ex;
+
+                while (ex2 != null)
+                {
+                    Console.WriteLine($"Error: {ex2.Message}");
+                    Console.WriteLine($"Stack Trace: {ex2.StackTrace}");
+                    Console.WriteLine();
+
+                    ex2 = ex2.InnerException;
+                }
+                Console.ResetColor();
+
+                Console.ReadKey();
+            }
         }
+
     }
 }
