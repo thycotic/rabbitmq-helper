a1cdedfb92bb2aced06b9b937ef52ca80492d28e
Dobroslav Kolev <dkolev@compfreak.com>
2018-05-24 16:58:37 -0400

    Exception extensions.

---
+++ src/Thycotic.RabbitMq.Helper.Logic/ExceptionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thycotic.RabbitMq.Helper.Logic
+{
+    /// <summary>
+    /// Exception extensions
+    /// </summary>
+    public static class ExceptionExtensions
+    {
+        /// <summary>
+        /// Gets the combined message of the exception and its inner exceptions.
+        /// </summary>
+        /// <param name="ex">The ex.</param>
+        /// <returns></returns>
+        public static string GetCombinedMessage(this Exception ex)
+        {
+            var ex2 = ex;
+            var sb = new StringBuilder();
+            while (ex2 != null)
+            {
+                sb.Append($"{ex2.Message};");
+                ex2 = ex2.InnerException;
+            }
+            return sb.ToString();
+        }
+    }
+}
