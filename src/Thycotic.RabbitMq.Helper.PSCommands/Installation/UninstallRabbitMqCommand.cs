e131646236c399c9e7f1cc6b7377a136e5b177d5
Dobroslav Kolev <dkolev@compfreak.com>
2018-05-24 16:33:22 -0400

    Verbiage fix.

--- src/Thycotic.RabbitMq.Helper.PSCommands/Installation/UninstallRabbitMqCommand.cs
+++ src/Thycotic.RabbitMq.Helper.PSCommands/Installation/UninstallRabbitMqCommand.cs
@@ -41,7 +41,7 @@ namespace Thycotic.RabbitMq.Helper.PSCommands.Installation
                 }
                 catch (Exception ex)
                 {
-                    throw new ApplicationException("Failed to stop RabbitMq. Manual creation might be necessary", ex);
+                    throw new ApplicationException("Failed to stop RabbitMq. Manual stop/uninstall might be necessary", ex);
                 }
             }
 
