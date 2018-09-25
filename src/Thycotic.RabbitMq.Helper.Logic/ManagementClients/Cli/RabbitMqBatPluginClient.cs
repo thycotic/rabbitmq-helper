a47cda291a28a04d66a83ff4ecdff21050c86328
Dobroslav Kolev <dobroslav.kolev@thycotic.com>
2018-09-25 13:43:59 -0400

    Enable/disable federation plugin. Fix output validation.

--- src/Thycotic.RabbitMq.Helper.Logic/ManagementClients/Cli/RabbitMqBatPluginClient.cs
+++ src/Thycotic.RabbitMq.Helper.Logic/ManagementClients/Cli/RabbitMqBatPluginClient.cs
@@ -9,7 +9,7 @@ namespace Thycotic.RabbitMq.Helper.Logic.ManagementClients.Cli
     /// <seealso cref="IProcessInteractor" />
     public class RabbitMqBatPluginClient : RabbitMqBatClient
     {
-       
+
         /// <summary>
         ///     Gets the executable.
         /// </summary>
@@ -20,18 +20,132 @@ namespace Thycotic.RabbitMq.Helper.Logic.ManagementClients.Cli
 
 
         /// <summary>
-        /// Enables the management console.
+        /// Enables the management UI.
         /// </summary>
         /// <exception cref="Exception"></exception>
-        public string EnableManagementConsole()
+        public string EnableManagementUi()
         {
             const string parameters2 = "enable rabbitmq_management";
 
             var output = Invoke(parameters2, TimeSpan.FromSeconds(60));
 
-            ValidateOutput("Plugin configuration unchanged.", output, false);
+            ValidateOutput($"Enabling plugins on node rabbit@{Environment.MachineName}:", output, false);
+            try
+            {
+                ValidateOutput("The following plugins have been enabled:", output, false);
+                ValidateOutput("rabbitmq_management", output, false);
+                ValidateOutput("rabbitmq_management_agent", output, false);
+                ValidateOutput("rabbitmq_web_dispatch", output, false);
+            }
+            catch (Exception)
+            {
+                ValidateOutput("Plugin configuration unchanged.", output, false);
+            }
 
             return output;
+        }
+
+        /// <summary>
+        /// Enables the management UI.
+        /// </summary>
+        /// <exception cref="Exception"></exception>
+        public string DisableManagementUi()
+        {
+            const string parameters2 = "disable rabbitmq_management";
+
+            var output = Invoke(parameters2, TimeSpan.FromSeconds(60));
+
+            ValidateOutput($"Disabling plugins on node rabbit@{Environment.MachineName}:", output, false);
+            try
+            {
+                ValidateOutput("The following plugins have been disabled:", output, false);
+                ValidateOutput("rabbitmq_management", output, false);
+                ValidateOutput("rabbitmq_management_agent", output, false);
+                ValidateOutput("rabbitmq_web_dispatch", output, false);
+            }
+            catch (Exception)
+            {
+                ValidateOutput("Plugin configuration unchanged.", output, false);
+            }
+
+            return output;
+        }
+
+        /// <summary>
+        /// Enables the federation and management UI.
+        /// </summary>
+        /// <returns></returns>
+        public string EnableFederationAndManagementUi()
+        {
+            const string parameters = "enable rabbitmq_federation";
+
+            var output = Invoke(parameters, TimeSpan.FromSeconds(60));
+
+            ValidateOutput($"Enabling plugins on node rabbit@{Environment.MachineName}:", output, false);
+            try
+            {
+                ValidateOutput("The following plugins have been enabled:", output, false);
+                ValidateOutput("rabbitmq_federation", output, false);
+            }
+            catch (Exception)
+            {
+                ValidateOutput("Plugin configuration unchanged.", output, false);
+            }
+
+            const string parameters2 = "enable rabbitmq_federation_management";
+
+            var output2 = Invoke(parameters2, TimeSpan.FromSeconds(60));
+
+            ValidateOutput($"Enabling plugins on node rabbit@{Environment.MachineName}:", output2, false);
+            try
+            {
+                ValidateOutput("The following plugins have been enabled:", output2, false);
+                ValidateOutput("rabbitmq_federation_management", output2, false);
+            }
+            catch (Exception)
+            {
+                ValidateOutput("Plugin configuration unchanged.", output, false);
+            }
+            return output + output2;
+        }
+
+        /// <summary>
+        /// Disables the federation and management UI.
+        /// </summary>
+        /// <returns></returns>
+        public string DisableFederationAndManagementUi()
+        {
+            const string parameters = "disable rabbitmq_federation_management";
+
+            var output = Invoke(parameters, TimeSpan.FromSeconds(60));
+
+            ValidateOutput($"Disabling plugins on node rabbit@{Environment.MachineName}:", output, false);
+            try
+            {
+
+                ValidateOutput("The following plugins have been disabled:", output, false);
+                ValidateOutput("rabbitmq_federation_management", output, false);
+            }
+            catch (Exception)
+            {
+                ValidateOutput("Plugin configuration unchanged.", output, false);
+            }
+
+            const string parameters2 = "disable rabbitmq_federation";
+
+            var output2 = Invoke(parameters2, TimeSpan.FromSeconds(60));
+
+            ValidateOutput($"Disabling plugins on node rabbit@{Environment.MachineName}:", output2, false);
+            try
+            {
+                ValidateOutput("The following plugins have been disabled:", output2, false);
+                ValidateOutput("rabbitmq_federation", output2, false);
+            }
+            catch (Exception)
+            {
+                ValidateOutput("Plugin configuration unchanged.", output, false);
+            }
+            return output + output2;
         }
     }
 }
--- src/Thycotic.RabbitMq.Helper.PSCommands/Management/DisableRabbitMqManagementCommand.cs
+++ src/Thycotic.RabbitMq.Helper.PSCommands/Management/DisableRabbitMqManagementCommand.cs
@@ -1,24 +1,21 @@
-﻿using System;
-using System.IO;
-using System.Management.Automation;
-using Thycotic.RabbitMq.Helper.Logic;
+﻿using System.Management.Automation;
 using Thycotic.RabbitMq.Helper.Logic.ManagementClients.Cli;
-using Thycotic.RabbitMq.Helper.Logic.OS;
 
 namespace Thycotic.RabbitMq.Helper.PSCommands.Management
 {
     /// <summary>
-    ///     Enables the RabbitMq management UI (https://www.rabbitmq.com/management.html)
+    ///     Disables the RabbitMq management UI (https://www.rabbitmq.com/management.html)
     /// </summary>
-    /// <para type="synopsis"> Enables the RabbitMq management IO (https://www.rabbitmq.com/management.html)</para>
+    /// <para type="synopsis"> Disables the RabbitMq management IO (https://www.rabbitmq.com/management.html)</para>
     /// <para type="description"></para>
     /// <para type="link" uri="http://www.thycotic.com">Thycotic Software Ltd</para>
+    /// <para type="link">Enable-RabbitMqManagement</para>
     /// <example>
     ///     <para>PS C:\></para> 
     ///     <code>Enable-RabbitMqManagement</code>
     /// </example>
-    [Cmdlet(VerbsLifecycle.Enable, "RabbitMqManagement")]
-    public class EnableRabbitMqManagementCommand : Cmdlet
+    [Cmdlet(VerbsLifecycle.Disable, "RabbitMqManagement")]
+    public class DisableRabbitMqManagementCommand : Cmdlet
     {
         /// <summary>
         ///     Processes the record.
@@ -27,9 +24,9 @@ namespace Thycotic.RabbitMq.Helper.PSCommands.Management
         {
             var client = new RabbitMqBatPluginClient();
 
-            WriteVerbose("Enabling management UI");
+            WriteVerbose("Disabling management UI");
 
-            var output = client.EnableManagementUi();
+            var output = client.DisableManagementUi();
 
             WriteVerbose(output);
         }
